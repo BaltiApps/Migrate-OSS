8607ad34621bc9ddf93b8f2fa70300388470e816
SayantanRC <sayantan.kgp@gmail.com>
2018-11-02 01:50:29 +0530

    changes in prep.sh updated helper.

--- app/src/main/assets/backup_app_and_data.sh
+++ app/src/main/assets/backup_app_and_data.sh
@@ -17,17 +17,18 @@ if [ "$8" = true ]; then
 fi
 
 # backup apk
-touch "$2/$1.apk" 2>/dev/null
 cd $3; cp $4 "$2/$1.apk"
 if [ -e "$2/$1.apk" ]; then
     echo "Apk copied"
 fi
 
 # backup data
-if [ ! "$6" = "NULL" ] && [ -e "$5/$6" ]; then
-    chmod +r $5/$6 2>/dev/null
-    cd "$5"
-    $7 tar -vczpf "$2/$1.tar.gz" "$6"
-else
-    echo "Data path : $5/$6 does not exist"
+if [ ! "$6" = "NULL" ]; then
+    if [ -e "$5/$6" ]; then
+        chmod +r $5/$6 2>/dev/null
+        cd "$5"
+        $7 tar -vczpf "$2/$1.tar.gz" "$6"
+    else
+        echo "Data path : $5/$6 does not exist"
+    fi
 fi
