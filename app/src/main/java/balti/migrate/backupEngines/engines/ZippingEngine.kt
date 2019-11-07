0a031f982cc7a03579d7dd1bf3f95922b91f98f1
SayantanRC <sayantan.kgp@gmail.com>
2019-11-07 17:06:36 +0530

    revert to old zipping engine
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/engines/ZippingEngine.kt
+++ app/src/main/java/balti/migrate/backupEngines/engines/ZippingEngine.kt
@@ -4,173 +4,126 @@ import balti.migrate.R
 import balti.migrate.backupEngines.BackupServiceKotlin
 import balti.migrate.backupEngines.ParentBackupClass
 import balti.migrate.backupEngines.containers.BackupIntentData
-import balti.migrate.backupEngines.containers.ZipAppBatch
-import balti.migrate.utilities.CommonToolKotlin.Companion.ERR_ZIP_ENGINE_INIT
 import balti.migrate.utilities.CommonToolKotlin.Companion.ERR_ZIP_TRY_CATCH
 import balti.migrate.utilities.CommonToolKotlin.Companion.EXTRA_PROGRESS_TYPE_ZIP_PROGRESS
-import balti.migrate.utilities.CommonToolKotlin.Companion.FILE_FILE_LIST
-import balti.migrate.utilities.CommonToolKotlin.Companion.WARNING_FILELIST_MAKE
-import balti.migrate.utilities.ToolsNoContext
-import java.io.*
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.util.zip.CRC32
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
-class ZippingEngine(private val jobcode: Int, bd: BackupIntentData,
-                    private val miscExtras : ArrayList<File> = ArrayList(0),
-                    private val miscFiles : ArrayList<File> = ArrayList(0),
-                    private val zipName: String, private val zipBatch: ZipAppBatch? = null) : ParentBackupClass(bd, EXTRA_PROGRESS_TYPE_ZIP_PROGRESS) {
+class ZippingEngine(private val jobcode: Int,
+                    private val bd: BackupIntentData) : ParentBackupClass(bd, EXTRA_PROGRESS_TYPE_ZIP_PROGRESS) {
 
     private val zippedFiles = ArrayList<String>(0)
-    private val errors = ArrayList<String>(0)
-    private val warnings = ArrayList<String>(0)
+    private val zipErrors = ArrayList<String>(0)
 
-    private fun filterExisting(files: ArrayList<File>): ArrayList<File>{
-        var c = 0
-        while (c < files.size) {
-            files[c].let {
-                if (!it.exists()) files.remove(it)
-                else c++
+    private fun getAllFiles(directory: File, allFiles: ArrayList<File> = ArrayList(0)): ArrayList<File>{
+        if (!directory.isDirectory) return arrayListOf(directory)
+        else {
+            for (f in directory.listFiles()){
+                allFiles.add(f)
+                if (f.isDirectory) getAllFiles(f, allFiles)
             }
         }
-        return files
+        return allFiles
     }
 
     override fun doInBackground(vararg params: Any?): Any {
 
         try {
 
-            val fileList = File(actualDestination, FILE_FILE_LIST)
-            val allFiles = ArrayList<File>(0)
-            val fileListEntries = ArrayList<File>(0)
+            val title = getTitle(R.string.zipping_all_files)
 
-            try {
+            resetBroadcast(false, title)
 
-                val title = getTitle(R.string.making_file_list)
-                resetBroadcast(true, title)
+            val directory = File(actualDestination)
+            val zipFile = File("$actualDestination.zip")
 
-                allFiles.addAll(filterExisting(miscFiles))
+            if (zipFile.exists()) zipFile.delete()
+            val files = getAllFiles(directory)
+            val zipOutputStream = ZipOutputStream(FileOutputStream(zipFile))
 
-                zipBatch?.let {
-                    it.zipPackets.forEach { p ->
-                        fileListEntries.addAll(filterExisting(p.files))
+            for (i in 0 until files.size){
+
+                val file = files[i]
+
+                if (BackupServiceKotlin.cancelAll) break
+
+                val relativeFilePath = file.absolutePath.substring(directory.absolutePath.length + 1).let {
+                    if (it.endsWith("/") && file.isFile) it.substring(0, it.length - 1)
+                    else if (file.isDirectory && !it.endsWith("/")) "$it/"
+                    else it
+                }
+
+                val zipEntry: ZipEntry
+
+                if (file.isDirectory){
+                    zipEntry = ZipEntry(relativeFilePath)
+
+                    zipOutputStream.putNextEntry(zipEntry)
+                    zipOutputStream.closeEntry()
+                }
+                else {
+                    zipEntry = ZipEntry(relativeFilePath)
+
+                    val bufferedInputStream = BufferedInputStream(FileInputStream(file))
+                    var buffer = ByteArray(4096)
+                    var read: Int
+
+                    val crc32 = CRC32()
+
+                    while (true){
+                        read = bufferedInputStream.read(buffer)
+                        if (read > 0)
+                            crc32.update(buffer, 0, read)
+                        else break
                     }
-                }
 
-                fileListEntries.addAll(filterExisting(miscExtras))
+                    bufferedInputStream.close()
 
-                allFiles.addAll(fileListEntries)
+                    zipEntry.size = file.length()
+                    zipEntry.compressedSize = file.length()
+                    zipEntry.crc = crc32.value
+                    zipEntry.method = ZipEntry.STORED
 
-                BufferedWriter(FileWriter(fileList)).run {
-                    fileListEntries.forEach {
-                        write("${it.name}\n")
+                    zipOutputStream.putNextEntry(zipEntry)
+
+                    val fileInputStream = FileInputStream(file)
+                    buffer = ByteArray(4096)
+
+                    while (true){
+                        read = fileInputStream.read(buffer)
+                        if (read > 0)
+                            zipOutputStream.write(buffer, 0, read)
+                        else break
                     }
-                    close()
+
+                    zipOutputStream.closeEntry()
+                    fileInputStream.close()
+                    file.delete()
+
+                    broadcastProgress("", "zipped: ${file.name}", true,
+                            commonTools.getPercentage((i+1), files.size))
                 }
 
-                ToolsNoContext.copyFile(fileList, engineContext.cacheDir.absolutePath).let {
-                    if (it != "") throw Exception(it)
-                }
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-                warnings.add("$WARNING_FILELIST_MAKE: ${e.message}")
+                zippedFiles.add(relativeFilePath)
             }
 
-            try {
-
-                val title = getTitle(R.string.zipping_all_files)
-                resetBroadcast(false, title)
-
-                val directory = File(actualDestination)
-                val zipFile = File(directory, "$zipName.zip")
-
-                if (zipFile.exists()) zipFile.delete()
-                if (fileList.exists()) allFiles.add(fileList)
-
-                val zipOutputStream = ZipOutputStream(FileOutputStream(zipFile))
-
-                for (i in allFiles.indices) {
-
-                    val file = allFiles[i]
-
-                    if (BackupServiceKotlin.cancelAll) break
-
-                    val relativeFilePath = file.absolutePath.substring(directory.absolutePath.length + 1).let {
-                        if (it.endsWith("/") && file.isFile) it.substring(0, it.length - 1)
-                        else if (file.isDirectory && !it.endsWith("/")) "$it/"
-                        else it
-                    }
-
-                    val zipEntry: ZipEntry
-
-                    if (file.isDirectory) {
-                        zipEntry = ZipEntry(file.absolutePath)
-
-                        zipOutputStream.putNextEntry(zipEntry)
-                        zipOutputStream.closeEntry()
-                    } else {
-                        zipEntry = ZipEntry(file.absolutePath)
-
-                        val bufferedInputStream = BufferedInputStream(FileInputStream(file))
-                        var buffer = ByteArray(4096)
-                        var read: Int
-
-                        val crc32 = CRC32()
-
-                        while (true) {
-                            read = bufferedInputStream.read(buffer)
-                            if (read > 0)
-                                crc32.update(buffer, 0, read)
-                            else break
-                        }
-
-                        bufferedInputStream.close()
-
-                        zipEntry.size = file.length()
-                        zipEntry.compressedSize = file.length()
-                        zipEntry.crc = crc32.value
-                        zipEntry.method = ZipEntry.STORED
-
-                        zipOutputStream.putNextEntry(zipEntry)
-
-                        val fileInputStream = FileInputStream(file)
-                        buffer = ByteArray(4096)
-
-                        while (true) {
-                            read = fileInputStream.read(buffer)
-                            if (read > 0)
-                                zipOutputStream.write(buffer, 0, read)
-                            else break
-                        }
-
-                        zipOutputStream.closeEntry()
-                        fileInputStream.close()
-                        file.delete()
-
-                        broadcastProgress("", "zipped: ${file.name}", true,
-                                commonTools.getPercentage((i + 1), allFiles.size))
-                    }
-
-                    zippedFiles.add(relativeFilePath)
-                }
-
-                zipOutputStream.close()
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-                errors.add("$ERR_ZIP_TRY_CATCH: ${e.message}")
-            }
+            zipOutputStream.close()
+            commonTools.dirDelete(directory.absolutePath)
         }
         catch (e: Exception){
             e.printStackTrace()
-            errors.add("$ERR_ZIP_ENGINE_INIT: ${e.message}")
+            zipErrors.add("$ERR_ZIP_TRY_CATCH: ${e.message}")
         }
 
         return 0
     }
 
     override fun postExecuteFunction() {
-        onEngineTaskComplete.onComplete(jobcode, errors, warnings, zippedFiles)
+        onEngineTaskComplete.onComplete(jobcode, zipErrors, jobResults = zippedFiles)
     }
 }
