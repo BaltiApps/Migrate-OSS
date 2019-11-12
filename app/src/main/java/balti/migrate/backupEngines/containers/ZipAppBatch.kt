2b1368185cfe44492da5ac9efdcd6c8c5fdef308
SayantanRC <sayantan.kgp@gmail.com>
2019-11-13 03:46:38 +0530

    changed fillist for system apks
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/containers/ZipAppBatch.kt
+++ app/src/main/java/balti/migrate/backupEngines/containers/ZipAppBatch.kt
@@ -34,7 +34,10 @@ data class ZipAppBatch(val zipPackets: ArrayList<ZipAppPacket> = ArrayList(0)) {
         BufferedWriter(FileWriter(fl)).run {
             zipPackets.forEach { zp ->
                 zp.appFiles.forEach { af ->
-                    write("${af.name}\n")
+                    if (af.name.endsWith(".app") && af.isDirectory && !zp.appPacket_z.apkPath.startsWith("/data")) {
+                        write("${af.name}_sys\n")
+                    }
+                    else write("${af.name}\n")
                 }
             }
             extrasFiles.forEach {ef ->
