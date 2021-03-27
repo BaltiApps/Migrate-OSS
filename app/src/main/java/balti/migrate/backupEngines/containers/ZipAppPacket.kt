2908a96b20479b1fb56deb96b975f4460605a51d
SayantanRC <sayantan.kgp@gmail.com>
2021-03-28 01:23:02 +0530

    ZipAppPacket.kt -> FileX complete
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/containers/ZipAppPacket.kt
+++ app/src/main/java/balti/migrate/backupEngines/containers/ZipAppPacket.kt
@@ -1,16 +1,15 @@
 package balti.migrate.backupEngines.containers
 
+import balti.filex.FileX
 import balti.migrate.extraBackupsActivity.apps.containers.AppPacket
-import balti.module.baltitoolbox.functions.FileHandlers.getDirLength
-import java.io.File
 
-data class ZipAppPacket(val appPacket_z: AppPacket, val appFiles: ArrayList<File>) {
+data class ZipAppPacket(val appPacket_z: AppPacket, val appFiles: ArrayList<FileX>) {
     var zipPacketSize: Long = 0
     private set
 
     init {
         appFiles.forEach {
-            zipPacketSize += getDirLength(it)
+            zipPacketSize += it.getDirLength()
         }
     }
 }
