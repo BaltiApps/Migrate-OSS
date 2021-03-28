3aa96a005c5e0dac7cdb7ac9a28b1d743b83dc35
SayantanRC <sayantan.kgp@gmail.com>
2021-03-28 05:45:03 +0530

    ZipAppBatch.kt -> FileX
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/containers/ZipAppBatch.kt
+++ app/src/main/java/balti/migrate/backupEngines/containers/ZipAppBatch.kt
@@ -1,17 +1,15 @@
 package balti.migrate.backupEngines.containers
 
+import balti.filex.FileX
 import balti.migrate.utilities.CommonToolsKotlin.Companion.FILE_FILE_LIST
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
 
 data class ZipAppBatch(val zipAppPackets: ArrayList<ZipAppPacket> = ArrayList(0)) {
     var batchSystemSize : Long = 0
     var batchDataSize : Long = 0
     var zipFullSize : Long = 0
-    val extrasFiles: ArrayList<File> = ArrayList(0)
+    val extrasFiles: ArrayList<FileX> = ArrayList(0)
     var partName = ""
-    var fileList: File? = null
+    var fileList: FileX? = null
     init {
         zipAppPackets.forEach {
             batchDataSize += it.appPacket_z.dataSizeBytes
@@ -19,7 +17,7 @@ data class ZipAppBatch(val zipAppPackets: ArrayList<ZipAppPacket> = ArrayList(0)
             zipFullSize += it.zipPacketSize
         }
     }
-    fun addExtras(extras: ArrayList<File>){
+    fun addExtras(extras: ArrayList<FileX>){
         extrasFiles.clear()
         extrasFiles.addAll(extras)
         extrasFiles.forEach {
@@ -28,23 +26,24 @@ data class ZipAppBatch(val zipAppPackets: ArrayList<ZipAppPacket> = ArrayList(0)
         }
     }
     fun createFileList(destination: String){
-        val fl = File("$destination/$partName", FILE_FILE_LIST)
+        val fl = FileX.new("$destination/$partName", FILE_FILE_LIST)
 
         fl.delete()
-        BufferedWriter(FileWriter(fl)).run {
-            zipAppPackets.forEach { zp ->
-                zp.appFiles.forEach { af ->
-                    if (af.name.endsWith(".app") && af.isDirectory && zp.appPacket_z.isSystem) {
-                        write("${af.name}_sys\n")
+        fl.startWriting(object : FileX.Writer(){
+            override fun writeLines() {
+                zipAppPackets.forEach { zp ->
+                    zp.appFiles.forEach { af ->
+                        if (af.name.endsWith(".app") && af.isDirectory && zp.appPacket_z.isSystem) {
+                            writeLine("${af.name}_sys")
+                        }
+                        else writeLine("${af.name}")
                     }
-                    else write("${af.name}\n")
+                }
+                extrasFiles.forEach {ef ->
+                    writeLine("${ef.name}")
                 }
             }
-            extrasFiles.forEach {ef ->
-                write("${ef.name}\n")
-            }
-            close()
-        }
+        })
 
         fileList = fl
     }
