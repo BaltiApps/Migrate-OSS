7124223e14d0dc331f210a2c6e55db117c0d80ce
SayantanRC <sayantan.kgp@gmail.com>
2021-03-28 14:59:19 +0530

    BackupUtils.kt -> FileX
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/utils/BackupUtils.kt
+++ app/src/main/java/balti/migrate/backupEngines/utils/BackupUtils.kt
@@ -3,17 +3,14 @@ package balti.migrate.backupEngines.utils
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import balti.filex.FileX
 import balti.migrate.backupEngines.containers.BackupIntentData
 import balti.migrate.extraBackupsActivity.apps.containers.AppPacket
 import balti.migrate.utilities.CommonToolsKotlin.Companion.KB_DIVISION_SIZE
 import balti.migrate.utilities.CommonToolsKotlin.Companion.PACKAGE_NAMES_KNOWN
 import balti.migrate.utilities.constants.MtdConstants
 import org.json.JSONObject
-import java.io.File
 import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.FileWriter
-import java.io.FileOutputStream
 
 class BackupUtils {
 
@@ -46,8 +43,8 @@ class BackupUtils {
 
         val packageName = appPacket.PACKAGE_INFO.packageName
         val metadataFileName = "$packageName.json"
-        val actualDestination = File("${bd.destination}/${bd.backupName}")
-        val metadataFile = File(actualDestination, metadataFileName)
+        val actualDestination = FileX.new("${bd.destination}/${bd.backupName}")
+        val metadataFile = FileX.new("${bd.destination}/${bd.backupName}", metadataFileName)
 
         val jsonObject = JSONObject()
         jsonObject.apply {
@@ -76,9 +73,11 @@ class BackupUtils {
         actualDestination.mkdirs()
 
         return try {
-            val writer = BufferedWriter(FileWriter(metadataFile))
-            writer.write(jsonObject.toString(4))
-            writer.close()
+            val writer = metadataFile.startWriting(object : FileX.Writer(){
+                override fun writeLines() {
+                    writeString(jsonObject.toString(4))
+                }
+            })
             ""
         }
         catch (e: Exception){
@@ -87,61 +86,19 @@ class BackupUtils {
         }
     }
 
-    /*fun makeMetadataFile(isSystem: Boolean, appName: String, apkName: String,
-                         dataName: String, iconFileName: String?,
-                         version: String, permissions: Boolean,
-                         appPacket: AppPacket, bd: BackupIntentData,
-                         doBackupInstallers: Boolean, actualDestination: String,
-                         iconString: String? = null): String {
-
-        val packageName = appPacket.PACKAGE_INFO.packageName
-
-        val metadataFileName = "$packageName.json"
-        val metadataFile = File("${bd.destination}/${bd.backupName}/$metadataFileName")
-
-        val jsonObject = JSONObject()
-        jsonObject.apply {
-            put(MtdConstants.MTD_IS_SYSTEM, isSystem)
-            put(MtdConstants.MTD_APP_NAME, appName)
-            put(MtdConstants.MTD_PACKAGE_NAME, packageName)
-            put(MtdConstants.MTD_APK, apkName)
-            put(MtdConstants.MTD_DATA, dataName)
-            put(MtdConstants.MTD_VERSION, version)
-            put(MtdConstants.MTD_DATA_SIZE, appPacket.dataSize)
-            put(MtdConstants.MTD_SYSTEM_SIZE, appPacket.systemSize)
-            put(MtdConstants.MTD_PERMISSION, permissions)
-            when {
-                iconFileName != null -> put(MtdConstants.MTD_ICON_FILE_NAME, iconFileName)
-                iconString != null -> put(MtdConstants.MTD_APP_ICON, iconString)
-            }
-            put(MtdConstants.MTD_INSTALLER_NAME, if (doBackupInstallers) appPacket.installerName else "NULL")
-        }
-
-        File(actualDestination).mkdirs()
-
-        return try {
-            val writer = BufferedWriter(FileWriter(metadataFile))
-            writer.write(jsonObject.toString(4))
-            writer.close()
-            ""
-        }
-        catch (e: Exception){
-            e.printStackTrace()
-            e.message.toString()
-        }
-    }*/
-
     fun makeStringIconFile(packageName: String, iconString: String, actualDestination: String): String{
 
         val iconFileName = "$packageName.icon"
-        val iconFile = File("$actualDestination/$iconFileName")
+        val iconFile = FileX.new("$actualDestination/$iconFileName")
 
-        File(actualDestination).mkdirs()
+        FileX.new(actualDestination).mkdirs()
 
         try {
-            val writer = BufferedWriter(FileWriter(iconFile))
-            writer.write(iconString)
-            writer.close()
+            val writer = iconFile.startWriting(object : FileX.Writer(){
+                override fun writeLines() {
+                    writeString(iconString)
+                }
+            })
         }
         catch (e: Exception){
             e.printStackTrace()
@@ -153,14 +110,14 @@ class BackupUtils {
     fun makeNewIconFile(packageName: String, icon: Bitmap, actualDestination: String): String{
 
         val iconFileName = "$packageName.png"
-        val iconFile = File("$actualDestination/$iconFileName")
+        val iconFile = FileX.new("$actualDestination/$iconFileName")
 
-        File(actualDestination).mkdirs()
+        FileX.new(actualDestination).mkdirs()
 
         try {
-            val fos = FileOutputStream(iconFile)
+            val fos = iconFile.outputStream()
             icon.compress(Bitmap.CompressFormat.PNG, 80, fos)
-            fos.close()
+            fos?.close()
         }
         catch (e: Exception){
             e.printStackTrace()
@@ -170,10 +127,8 @@ class BackupUtils {
     }
 
     fun makePermissionFile(packageName: String, actualDestination: String, pm: PackageManager): String{
-        val backupPerm = File("$actualDestination/$packageName.perm")
+        val permFile = FileX.new("$actualDestination/$packageName.perm")
         try {
-
-            val writer = BufferedWriter(FileWriter(backupPerm))
 
             val pi = pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS)
             val perms = pi.requestedPermissions
@@ -188,12 +143,15 @@ class BackupUtils {
                 }
             }
 
-            if (granted.isEmpty()) writer.write("no_permissions_granted\n")
-            else granted.forEach {
-                writer.write("$it\n")
-            }
+            permFile.startWriting(object : FileX.Writer(){
+                override fun writeLines() {
+                    if (granted.isEmpty()) writeLine("no_permissions_granted")
+                    else granted.forEach {
+                        writeLine(it)
+                    }
+                }
+            })
 
-            writer.close()
             return ""
         } catch (e: Exception) {
             e.printStackTrace()
