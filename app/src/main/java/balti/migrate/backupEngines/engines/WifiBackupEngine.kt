35f31cf75ed3332594b4e7e62e4459d65d4971a1
SayantanRC <sayantan.kgp@gmail.com>
2021-03-28 16:41:59 +0530

    WifiBackupEngine.kt -> FileX
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/backupEngines/engines/WifiBackupEngine.kt
+++ app/src/main/java/balti/migrate/backupEngines/engines/WifiBackupEngine.kt
@@ -1,5 +1,6 @@
 package balti.migrate.backupEngines.engines
 
+import balti.filex.FileX
 import balti.migrate.R
 import balti.migrate.backupEngines.BackupServiceKotlin
 import balti.migrate.backupEngines.ParentBackupClass
@@ -7,22 +8,19 @@ import balti.migrate.backupEngines.containers.BackupIntentData
 import balti.migrate.extraBackupsActivity.wifi.containers.WifiDataPacket
 import balti.migrate.utilities.CommonToolsKotlin.Companion.ERR_WIFI_TRY_CATCH
 import balti.migrate.utilities.CommonToolsKotlin.Companion.EXTRA_PROGRESS_TYPE_WIFI
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
 
 class WifiBackupEngine(private val jobcode: Int,
                        private val bd: BackupIntentData,
                        private val wifiDataPacket: WifiDataPacket) : ParentBackupClass(bd, EXTRA_PROGRESS_TYPE_WIFI) {
 
-    private val wifiFile by lazy { File(actualDestination, wifiDataPacket.fileName) }
+    private val wifiFile by lazy { FileX.new(actualDestination, wifiDataPacket.fileName) }
     private val errors by lazy { ArrayList<String>(0) }
 
     override suspend fun doInBackground(arg: Any?): Any? {
 
         try {
 
-            File(actualDestination).mkdirs()
+            FileX.new(actualDestination).mkdirs()
             if (wifiFile.exists()) wifiFile.delete()
 
             val title = getTitle(R.string.storing_wifi_data)
@@ -30,19 +28,16 @@ class WifiBackupEngine(private val jobcode: Int,
             resetBroadcast(true, title)
 
             heavyTask {
-                BufferedWriter(FileWriter(wifiFile, true)).run {
-
-                    wifiDataPacket.contents.let { contents ->
-                        for (i in 0 until contents.size) {
-
-                            if (BackupServiceKotlin.cancelAll) break
-
-                            this.write(contents[i])
+                wifiFile.startWriting(object : FileX.Writer(){
+                    override fun writeLines() {
+                        wifiDataPacket.contents.let { contents ->
+                            for (i in 0 until contents.size) {
+                                if (BackupServiceKotlin.cancelAll) break
+                                write(contents[i])
+                            }
                         }
                     }
-
-                    this.close()
-                }
+                })
             }
         }
         catch (e: Exception){
