cec4f9a7f12b046dcfe635e49979da1a74d2d5b8
SayantanRC <sayantan.kgp@gmail.com>
2021-04-02 05:27:01 +0530

    AddonReceiver.kt: route MDP su operations to MDP_Setup.kt
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/utilities/AddonReceiver.kt
+++ app/src/main/java/balti/migrate/utilities/AddonReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import balti.migrate.utilities.constants.MDP_Constants
 import balti.migrate.utilities.constants.MDP_Constants.Companion.ACTION_TO_MDP_ENGINE
+import balti.migrate.utilities.constants.MDP_Constants.Companion.ACTION_TO_MDP_SETUP
 
 class AddonReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -18,6 +19,7 @@ class AddonReceiver: BroadcastReceiver() {
                             when (opType) {
                                 MDP_Constants.EXTRA_OPERATION_BACKUP_PROGRESS -> Intent(ACTION_TO_MDP_ENGINE)
                                 MDP_Constants.EXTRA_OPERATION_BACKUP_DONE -> Intent(ACTION_TO_MDP_ENGINE)
+                                MDP_Constants.EXTRA_OPERATION_DUMMY_SU -> Intent(ACTION_TO_MDP_SETUP)
                                 else -> Intent()
                             }.putExtras(this)
                     )
