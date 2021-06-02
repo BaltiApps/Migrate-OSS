2e4cf4a9ee7e4b439cf0e16ee5ffa1d48bb92714
SayantanRC <sayantan.kgp@gmail.com>
2021-06-03 01:59:34 +0530

    StorageType.kt: enum class to denote which storage type is being used
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

---
+++ app/src/main/java/balti/migrate/storageSelector/StorageType.kt
@@ -0,0 +1,7 @@
+package balti.migrate.storageSelector
+
+enum class StorageType(val value: String) {
+    SAF("SAF_STORAGE"),
+    CONVENTIONAL("CONVENTIONAL_STORAGE"),
+    ALL_FILES_STORAGE("ALL_FILES_STORAGE"),
+}
