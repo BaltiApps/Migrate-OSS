a34b4a77525f630a67cda6be1cfc42c76848305a
SayantanRC <sayantan.kgp@gmail.com>
2021-06-03 02:11:25 +0530

    StorageSelectorActivity.kt: Copy many logic for SAF request from AskForName.kt
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/storageSelector/StorageSelectorActivity.kt
+++ app/src/main/java/balti/migrate/storageSelector/StorageSelectorActivity.kt
@@ -1,17 +1,29 @@
 package balti.migrate.storageSelector
 
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import balti.filex.FileX
+import balti.filex.FileXInit
 import balti.migrate.R
+import balti.migrate.utilities.CommonToolsKotlin
+import balti.migrate.utilities.CommonToolsKotlin.Companion.DEFAULT_INTERNAL_STORAGE_DIR
+import balti.migrate.utilities.CommonToolsKotlin.Companion.PREF_DEFAULT_BACKUP_PATH
+import balti.migrate.utilities.CommonToolsKotlin.Companion.PREF_STORAGE_TYPE
+import balti.module.baltitoolbox.functions.SharedPrefs.putPrefString
 import kotlinx.android.synthetic.main.storage_selector.*
 
 
 class StorageSelectorActivity: AppCompatActivity() {
 
     private val ALL_FILES_ACCESS_PERMISSION = "android.permission.MANAGE_EXTERNAL_STORAGE"
+    private var isOnlySafAvailable = false
+    private val defaultInternalStorage = DEFAULT_INTERNAL_STORAGE_DIR
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +33,76 @@ class StorageSelectorActivity: AppCompatActivity() {
         setFinishOnTouchOutside(false)
 
         getManifestPermissions().run {
-            if (contains(ALL_FILES_ACCESS_PERMISSION)){
-                storage_select_all_files_access.visibility = View.VISIBLE
-                storage_select_conventional.visibility = View.GONE
-            }
-            else {
+
+            // 1st case for all older androids where SAF is avoided
+            if (CommonToolsKotlin.ALLOW_CONVENTIONAL_STORAGE) {
                 storage_select_all_files_access.visibility = View.GONE
                 storage_select_conventional.visibility = View.VISIBLE
             }
+
+            // 2nd case: This will be false if Google decides to deny all files access.
+            // In that case the permission needs to be removed from manfest, and the check will be false.
+            else if (contains(ALL_FILES_ACCESS_PERMISSION)){
+                storage_select_all_files_access.visibility = View.VISIBLE
+                storage_select_conventional.visibility = View.GONE
+            }
+
+            // 3rd case: For 2nd case fail i.e. conventional storage not possible, all files access denied by Google.
+            // Only option is SAF.
+            else {
+                isOnlySafAvailable = true
+                storage_select_root_view.visibility = View.GONE
+                safStorageRequest()
+            }
         }
+
+        storage_select_saf.setOnClickListener {
+            safStorageRequest()
+        }
+    }
+
+    override fun onBackPressed() {
+        sendResult(false)
+    }
+
+    private val onCancelDialogListener = DialogInterface.OnClickListener { _, _ -> if (isOnlySafAvailable) sendResult(false) }
+
+    private fun safStorageRequest(){
+
+        fun safStorageValidator(){
+            FileXInit.setTraditional(false)
+            FileXInit.requestUserPermission(reRequest = true) { resultCode, data ->
+
+                if (resultCode == Activity.RESULT_OK) {
+
+                    val root = FileX.new("/")
+
+                    if (root.volumePath.isNullOrBlank()) {
+                        AlertDialog.Builder(this).apply {
+                            setCancelable(false)
+                            setTitle(R.string.this_location_cannot_be_selected)
+                            setMessage(R.string.this_location_cannot_be_selected_desc)
+                            setPositiveButton(R.string.proceed) { _, _ ->
+                                safStorageValidator()
+                            }
+                            setNegativeButton(android.R.string.cancel, onCancelDialogListener)
+                        }.show()
+
+                    } else {
+                        sendResult(true, StorageType.SAF, root.canonicalPath)
+                    }
+                }
+            }
+        }
+
+        AlertDialog.Builder(this).apply {
+            setTitle(R.string.choose_storage_location)
+            setMessage(R.string.choose_storage_location_desc_saf)
+            setPositiveButton(R.string.proceed) { _, _ -> safStorageValidator() }
+            setNeutralButton(android.R.string.cancel, onCancelDialogListener)
+            setCancelable(false)
+        }
+                .show()
     }
 
     private fun getManifestPermissions(): List<String>{
@@ -45,5 +118,17 @@ class StorageSelectorActivity: AppCompatActivity() {
             e.printStackTrace()
         }
         return manifestPermissions
+    }
+
+    private fun sendResult(success: Boolean = false, storageType: StorageType? = null, storagePath: String = defaultInternalStorage){
+        if (success){
+            storageType?.value?.let { putPrefString(PREF_STORAGE_TYPE, it) }
+            putPrefString(PREF_DEFAULT_BACKUP_PATH, storagePath)
+            setResult(Activity.RESULT_OK)
+        }
+        else {
+            setResult(Activity.RESULT_CANCELED)
+        }
+        finish()
     }
 }
