a98a79345cb94316dc1d11dbe390d5b444f78d2c
SayantanRC <sayantan.kgp@gmail.com>
2021-06-09 05:56:40 +0530

    AllFilesAccessHandler.kt: Add comments
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/java/balti/migrate/storageSelector/AllFilesAccessHandler.kt
+++ app/src/main/java/balti/migrate/storageSelector/AllFilesAccessHandler.kt
@@ -11,8 +11,11 @@ import balti.module.baltitoolbox.functions.SharedPrefs.getPrefString
 
 class AllFilesAccessHandler(private val context: Context, private val defaultInternalStorage: String) {
 
+    // Final path to be sent to activity.
     private var finalPath = defaultInternalStorage
 
+    // Alert dialog which allows to choose a custom location.
+    // Also shows the selected path.
     private val chooserDialog by lazy {
         AlertDialog.Builder(context).apply {
             setTitle(R.string.all_files_access_custom_location)
@@ -24,7 +27,7 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
         }.create()
     }
 
-
+    // Returns true if a path is same as /sdcard/Migrate
     private fun isPathSameAsDefault(pathToCheck: String): Boolean {
         return FileX.new(pathToCheck, true).let {
             val defaultInternalStorageFile = FileX.new(defaultInternalStorage, true)
@@ -32,10 +35,16 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
         }
     }
 
+    // Method to update the message of chooserDialog
+    // newPath: A string path to be set in the message of chooserDialog.
+    //          Leave blank to get the last selected path from shared preference.
     private fun setChooserDialogMessage(newPath: String? = null) {
 
         val pathToCheck = newPath ?: getPrefString(PREF_DEFAULT_BACKUP_PATH, defaultInternalStorage)
 
+        // If path is /sdcard/Migrate, display the path as [Internal storage]/Migrate.
+        // Else show the raw path.
+        // Also set the finalPath class variable.
         val displayLocation: String =
                 if (isPathSameAsDefault(pathToCheck)) {
                     finalPath = defaultInternalStorage
@@ -46,7 +55,7 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
                     }
                 }
 
-
+        // create the final message and set to chooserDialog
         val message =
                 context.getString(R.string.all_files_access_custom_location_desc1) + "\n\n" +
                 displayLocation + "\n\n" +
@@ -54,6 +63,9 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
         chooserDialog.setMessage(message)
     }
 
+    // Simple dialog to be shown if the selected custom path is invalid.
+    // A custom path is invalid if it cannot be written to by a traditional FileX object.
+    // Path is usually inaccessible if it is not in Internal storage or SD-CARD.
     private fun showInvalidLocation(){
         AlertDialog.Builder(context).apply {
             setCancelable(false)
@@ -63,7 +75,9 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
         }.show()
     }
 
-    private fun startChooser(){
+    // Method to start System UI file picker to select a custom path.
+    // Also validates the path and set the message in chooserDialog.
+    private fun openDocumentsUIForPath(){
         FileXInit.setTraditional(false)
         FileXInit.requestUserPermission(reRequest = true){ resultCode, _ ->
             if (resultCode == Activity.RESULT_OK){
@@ -80,22 +94,36 @@ class AllFilesAccessHandler(private val context: Context, private val defaultInt
         }
     }
 
+    // Public method to actually start showing the chooserDialog.
+    // onSuccess: Method to execute when the "OK" button is pressed on chooserDialog.
     fun showCustomStorageDialog(onSuccess: (finalPath: String) -> Unit){
         chooserDialog.setOnShowListener {
             chooserDialog.apply {
+
+                // buttons are being set after dialog is shown to
+                // prevent the dialog from being dismissed on button press.
                 setChooserDialogMessage()
+
+                // Button "Choose custom"
                 getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener {
-                    startChooser()
+                    openDocumentsUIForPath()
                 }
+
+                // Button "Use default"
                 getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener {
                     setChooserDialogMessage(defaultInternalStorage)
                 }
+
+                // Button "OK"
                 getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    // dismiss and send finalPath class variable
                     dismiss()
                     onSuccess(finalPath)
                 }
             }
         }
+
+        // Finally show the dialog.
         chooserDialog.show()
     }
 }
