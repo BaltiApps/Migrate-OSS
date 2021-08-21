246ea3d494e2a8d8ede6d6b3645ccd712f6db37f
SayantanRC <sayantan_rc@protonmail.com>
2021-08-22 03:50:09 +0530

    ContactsFragment.kt: Use Activity contract to ask for contacts permission. Then read contacts using the new ReadContactsKotlin class.

--- app/src/main/java/balti/migrate/extraBackupsActivity/contacts/ContactsFragment.kt
+++ app/src/main/java/balti/migrate/extraBackupsActivity/contacts/ContactsFragment.kt
@@ -2,17 +2,47 @@ package balti.migrate.extraBackupsActivity.contacts
 
 import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
+import balti.migrate.AppInstance.Companion.contactsList
 import balti.migrate.R
 import balti.migrate.extraBackupsActivity.ExtrasParentFragment
-import balti.migrate.utilities.CommonToolsKotlin.Companion.CONTACT_PERMISSION
-import kotlinx.android.synthetic.main.extra_fragment_contacts.view.*
+import balti.migrate.extraBackupsActivity.ExtrasParentReader
+import balti.module.baltitoolbox.functions.Misc.runSuspendFunction
+import kotlinx.android.synthetic.main.extra_fragment_contacts.*
 
 class ContactsFragment: ExtrasParentFragment(R.layout.extra_fragment_contacts) {
 
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
+    override lateinit var readTask: ExtrasParentReader
+
+    override fun onCreateFragment() {
+        super.onCreateFragment()
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+               startReadTask()
+            }
+            else {
+                Toast.makeText(mActivity, R.string.contacts_access_needed, Toast.LENGTH_SHORT).show()
+                contacts_fragment_checkbox.isChecked = false
+            }
+        }
+    }
+
+    private fun startReadTask(){
+        readTask = ReadContactsKotlin(this)
+
+        runSuspendFunction {
+            val results = readTask.executeWithResult()
+        }
+    }
+
     override fun onCreateView(savedInstanceState: Bundle?) {
-        rootView.contacts_fragment_checkbox?.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        contacts_fragment_checkbox.setOnCheckedChangeListener { buttonView, isChecked ->
 
             mActivity?.run {
 
@@ -24,19 +54,23 @@ class ContactsFragment: ExtrasParentFragment(R.layout.extra_fragment_contacts) {
                             buttonView.isChecked = false
                         }
                         .setNegativeButton(R.string.backup_contacts_anyway) { _, _ ->
-                            ActivityCompat.requestPermissions(
-                                this,
-                                arrayOf(Manifest.permission.READ_CONTACTS), CONTACT_PERMISSION
-                            )
+                            permissionLauncher.launch(Manifest.permission.READ_CONTACTS)
                         }
                         .setCancelable(false)
                         .show()
                 }
+                else deselectExtra(contactsList, listOf(contacts_read_progress, contacts_read_text_status))
             }
         }
+
+
     }
 
+    override val viewIdStatusText: Int = R.id.contacts_read_text_status
+    override val viewIdProgressBar: Int = R.id.contacts_read_progress
+    override val viewIdCheckbox: Int = R.id.contacts_fragment_checkbox
+
     override fun isChecked(): Boolean? = try {
-        rootView.contacts_fragment_checkbox?.isChecked
+        contacts_fragment_checkbox?.isChecked
     } catch (e: Exception) { null }
 }
