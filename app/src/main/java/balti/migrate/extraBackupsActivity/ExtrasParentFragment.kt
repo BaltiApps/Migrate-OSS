33497e8906de196199dc0e6fc28714088ca1a35d
SayantanRC <sayantan_rc@protonmail.com>
2021-08-22 03:35:06 +0530

    ExtrasParentFragment.kt: Add delegate views for status text, progress bar, checkbox and the main item

--- app/src/main/java/balti/migrate/extraBackupsActivity/ExtrasParentFragment.kt
+++ app/src/main/java/balti/migrate/extraBackupsActivity/ExtrasParentFragment.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import balti.module.baltitoolbox.functions.Misc.tryIt
 
 abstract class ExtrasParentFragment(layoutId: Int): Fragment() {
 
-    val rootView: View by lazy { View.inflate(activity, layoutId, null) }
+    private val rootView: LinearLayout by lazy { View.inflate(activity, layoutId, null) as LinearLayout }
     var mActivity: Activity? = null
+
+    var delegateMainItem: LinearLayout? = null
+    var delegateStatusText: TextView? = null
+    var delegateProgressBar: ProgressBar? = null
+    var delegateCheckbox: CheckBox? = null
 
     final override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        rootView
         onCreateFragment()
         return rootView
     }
@@ -29,15 +40,39 @@ abstract class ExtrasParentFragment(layoutId: Int): Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        delegateMainItem = rootView
+        delegateStatusText = getViewOrNull(viewIdStatusText)
+        delegateProgressBar = getViewOrNull(viewIdProgressBar)
+        delegateCheckbox = getViewOrNull(viewIdCheckbox)
         mActivity = context as Activity
     }
 
     override fun onDetach() {
+        delegateMainItem = null
+        delegateStatusText = null
+        delegateProgressBar = null
+        delegateCheckbox = null
         super.onDetach()
         mActivity = null
+    }
+
+    private fun <T: View> getViewOrNull(viewId: Int): T? {
+        return try { rootView.findViewById(viewId) } catch (_: Exception) { null }
+    }
+
+    protected fun deselectExtra(dataContainer: ArrayList<*>?, viewsToHide: List<View>? = null, viewsToShow: List<View>? = null) {
+        dataContainer?.clear()
+        delegateMainItem?.isClickable = false
+        viewsToHide?.forEach { it.visibility = View.GONE }
+        viewsToShow?.forEach { it.visibility = View.VISIBLE }    //usually includes Stock Android recommended label
+        tryIt { readTask.cancel() }
     }
 
     open fun onCreateFragment(){}
     abstract fun onCreateView(savedInstanceState: Bundle?)
     abstract fun isChecked(): Boolean?
+    abstract val readTask: ExtrasParentReader
+    abstract val viewIdStatusText: Int
+    abstract val viewIdProgressBar: Int
+    abstract val viewIdCheckbox: Int
 }
