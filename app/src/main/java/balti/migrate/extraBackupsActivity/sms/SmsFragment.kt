52adf24188c80f6d15b182cc5b33ef0cd9ed9716
SayantanRC <sayantan_rc@protonmail.com>
2021-08-24 19:25:19 +0530

    SmsFragment.kt: complete the class

--- app/src/main/java/balti/migrate/extraBackupsActivity/sms/SmsFragment.kt
+++ app/src/main/java/balti/migrate/extraBackupsActivity/sms/SmsFragment.kt
@@ -1,13 +1,24 @@
 package balti.migrate.extraBackupsActivity.sms
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import balti.migrate.AppInstance.Companion.smsList
 import balti.migrate.R
 import balti.migrate.extraBackupsActivity.ParentFragmentForExtras
 import balti.migrate.extraBackupsActivity.ParentReaderForExtras
+import balti.migrate.extraBackupsActivity.ReaderJobResultHolder
+import balti.migrate.extraBackupsActivity.sms.containers.SmsDataPacketKotlin
+import balti.module.baltitoolbox.functions.Misc.runOnMainThread
+import balti.module.baltitoolbox.functions.Misc.runSuspendFunction
+import balti.module.baltitoolbox.functions.Misc.showErrorDialog
+import balti.module.baltitoolbox.functions.Misc.tryIt
 import kotlinx.android.synthetic.main.extra_fragment_sms.*
 
 class SmsFragment: ParentFragmentForExtras(R.layout.extra_fragment_sms) {
@@ -24,15 +35,93 @@ class SmsFragment: ParentFragmentForExtras(R.layout.extra_fragment_sms) {
                 startReadTask()
             }
             else {
-                Toast.makeText(mActivity, R.string.calls_access_needed, Toast.LENGTH_SHORT).show()
+                Toast.makeText(mActivity, R.string.sms_access_needed, Toast.LENGTH_SHORT).show()
                 sms_fragment_checkbox.isChecked = false
             }
         }
         selectorLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             if (it.resultCode == Activity.RESULT_OK){
-                updateCalls()
+                updateSms()
             }
         }
     }
+
+    override fun onCreateView(savedInstanceState: Bundle?) {
+
+        delegateCheckbox?.setOnCheckedChangeListener { _, isChecked ->
+
+            mActivity?.run {
+
+                if (isChecked){
+                    permissionLauncher.launch(Manifest.permission.READ_SMS)
+                }
+                else deselectExtra(smsList, listOf(delegateProgressBar, delegateStatusText))
+            }
+        }
+    }
+
+    private fun startReadTask(){
+        readTask = ReadSmsKotlin(this)
+
+        runSuspendFunction {
+            val jobResults = readTask.executeWithResult() as ReaderJobResultHolder
+            if (jobResults.success) {
+                runOnMainThread {
+                    tryIt({
+                        updateSms(jobResults.result as ArrayList<SmsDataPacketKotlin>)
+                    }, true)
+                }
+            } else {
+                delegateCheckbox?.isChecked = false
+                showErrorDialog(
+                    jobResults.result.toString(),
+                    getString(R.string.error_reading_sms)
+                )
+            }
+        }
+    }
+
+    private fun updateSms(newList: ArrayList<SmsDataPacketKotlin> = smsList){
+
+        smsList.apply {
+            if (newList != this){
+                clear()
+                addAll(newList)
+            }
+        }
+
+        delegateStatusText?.text = getString(R.string.reading)
+        delegateProgressBar?.visibility = View.GONE
+
+        var n = 0
+        newList.forEach {
+            if (it.selected) n++
+        }
+
+        if (smsList.size > 0){
+            delegateStatusText?.text = "$n ${getString(R.string.of)} ${smsList.size}"
+            delegateMainItem?.setOnClickListener {
+                //LoadSmsForSelectionKotlin_legacy(JOBCODE_LOAD_SMS, this, smsList).execute()
+                selectorLauncher.launch(Intent(mActivity, LoadSmsForSelection::class.java))
+            }
+        }
+        else {
+            mActivity?.let {
+                AlertDialog.Builder(it)
+                    .setMessage(R.string.empty_sms)
+                    .setPositiveButton(R.string.close) {_, _ -> delegateCheckbox?.isChecked = false }
+                    .setCancelable(false)
+                    .show()
+            }
+        }
+    }
+
+    override val viewIdStatusText: Int = R.id.sms_read_text_status
+    override val viewIdProgressBar: Int = R.id.sms_read_progress
+    override val viewIdCheckbox: Int = R.id.sms_fragment_checkbox
+
+    override fun isChecked(): Boolean? = try {
+        delegateCheckbox?.isChecked
+    } catch (e: Exception) { null }
 
 }
