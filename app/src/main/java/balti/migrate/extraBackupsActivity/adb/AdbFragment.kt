985323d8da74311499fdb77db3b75810c90393aa
SayantanRC <sayantan_rc@protonmail.com>
2021-09-26 03:38:34 +0530

    [WIP] AdbFragment.kt: Create class. Currently empty.

---
+++ app/src/main/java/balti/migrate/extraBackupsActivity/adb/AdbFragment.kt
@@ -0,0 +1,7 @@
+package balti.migrate.extraBackupsActivity.adb
+
+import balti.migrate.R
+import balti.migrate.extraBackupsActivity.ParentFragmentForExtras
+
+class AdbFragment: ParentFragmentForExtras(R.layout.extra_fragment_adb) {
+}
