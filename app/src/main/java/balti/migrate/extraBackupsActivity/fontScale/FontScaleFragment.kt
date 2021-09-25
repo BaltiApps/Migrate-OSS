c60a99e300a1e4335c68a587e994e522fe1ca342
SayantanRC <sayantan_rc@protonmail.com>
2021-09-25 12:48:28 +0530

    [WIP] FontScaleFragment.kt: Declare class. Empty right now.

---
+++ app/src/main/java/balti/migrate/extraBackupsActivity/fontScale/FontScaleFragment.kt
@@ -0,0 +1,8 @@
+package balti.migrate.extraBackupsActivity.fontScale
+
+import balti.migrate.R
+import balti.migrate.extraBackupsActivity.ParentFragmentForExtras
+
+class FontScaleFragment: ParentFragmentForExtras(R.layout.extra_fragment_font_scale) {
+
+}
