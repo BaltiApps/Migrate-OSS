02b8bf4c67a60aa79d39507c0f75a78442b5a0e8
SayantanRC <sayantan_rc@protonmail.com>
2021-09-26 23:13:28 +0530

    InstallersFragment.kt: Fill the class.

--- app/src/main/java/balti/migrate/extraBackupsActivity/installer/InstallersFragment.kt
+++ app/src/main/java/balti/migrate/extraBackupsActivity/installer/InstallersFragment.kt
@@ -1,4 +1,69 @@
 package balti.migrate.extraBackupsActivity.installer
 
-class InstallersFragment {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import balti.migrate.AppInstance
+import balti.migrate.R
+import balti.migrate.extraBackupsActivity.ParentFragmentForExtras
+import balti.migrate.extraBackupsActivity.ParentReaderForExtras
+import balti.migrate.extraBackupsActivity.engines.calls.LoadCallsForSelection
+
+class InstallersFragment: ParentFragmentForExtras(R.layout.extra_fragment_installers) {
+
+    override val readTask: ParentReaderForExtras? = null
+    private lateinit var selectorLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreateFragment() {
+        super.onCreateFragment()
+        selectorLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+                updateInstallerCount()
+            }
+        }
+    }
+
+    private fun startReadTask(){
+        AppInstance.appInstallers.apply {
+            clear()
+            AppInstance.selectedBackupDataPackets.forEach {
+                this[it.PACKAGE_INFO.packageName] = it.installerName
+            }
+        }
+    }
+
+    override fun onCreateView(savedInstanceState: Bundle?) {
+        delegateCheckbox?.setOnCheckedChangeListener { _, isChecked ->
+
+            mActivity?.run {
+
+                if (isChecked){
+                    startReadTask()
+                    updateInstallerCount()
+                    delegateMainItem?.setOnClickListener {
+                        selectorLauncher.launch(Intent(mActivity, LoadCallsForSelection::class.java))
+                    }
+                }
+                else {
+                    deselectExtra(null, listOf(delegateStatusText))
+                    AppInstance.appInstallers.clear()
+                }
+            }
+        }
+    }
+
+    private fun updateInstallerCount(){
+        var count = 0
+        AppInstance.appInstallers.forEach {
+            if (it.value.isNotBlank()) count++
+        }
+
+        delegateStatusText?.text = "$count ${getString(R.string.of)} ${AppInstance.selectedBackupDataPackets.size}"
+    }
+
+    override val viewIdStatusText: Int = R.id.keyboard_read_text_status
+    override val viewIdCheckbox: Int = R.id.keyboard_fragment_checkbox
+
 }
