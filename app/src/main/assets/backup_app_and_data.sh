4228b176c301ee8b8774a9af25db17d4f013968b
SayantanRC <sayantan_rc@protonmail.com>
2021-10-19 21:03:05 +0530

    AppBackupEngine.kt, backup_app_and_data.sh: Create methods to store apk names of apps in a separate file and parse the file to group them and store in AppInstance.appApkFiles

--- app/src/main/assets/backup_app_and_data.sh
+++ app/src/main/assets/backup_app_and_data.sh
@@ -1,7 +1,7 @@
 #!sbin/sh
 
-# PARAMETERS: packageName destination apkPath apkName dataPath dataName busyBox ignoreCache
-#                  1          2          3       4       5         6       7         8
+# PARAMETERS: packageName destination apkPath apkName dataPath dataName busyBox ignoreCache apkNames
+#                  1          2          3       4       5         6       7         8        9
 
 echo " "
 
@@ -13,6 +13,7 @@ DATA_PATH="$5"
 DATA_NAME="$6"
 BUSYBOX="$7"
 IGNORE_CACHE=$8
+APK_NAMES_LIST_FILE=$9
 
 if [[ ! -e "$DESTINATION" ]]; then
     echo "Destination for package $PACKAGE_NAME: $DESTINATION does not exist. Making..."
@@ -29,13 +30,37 @@ if [[ ${APK_PATH} != "NULL" && ${APK_NAME} != "NULL" ]]; then
     mkdir -p ${appDir}
 
     # backup apk
-    cd ${APK_PATH}; cp "$APK_NAME" "${appDir}/$PACKAGE_NAME.apk"
-    if [[ -e "$DESTINATION/$PACKAGE_NAME.app/$PACKAGE_NAME.apk" ]]; then
-        echo "Apk copied"
-    fi
+    #cd ${APK_PATH}; cp "$APK_NAME" "${appDir}/$PACKAGE_NAME.apk"
+    #if [[ -e "$DESTINATION/$PACKAGE_NAME.app/$PACKAGE_NAME.apk" ]]; then
+    #    echo "Apk copied"
+    #fi
 
     # copy split apks (new in v2.0)
-    cp ${APK_PATH}/split_*.apk "${appDir}/" 2>/dev/null && echo "Copied split apks"
+    #cp ${APK_PATH}/*.apk "${appDir}/" 2>/dev/null && echo "Copied split apks"
+
+    cd ${APK_PATH}
+
+    # Loop over apk files and copy them.
+    for f in *.apk; do
+
+        display_name=""
+
+        # Check if base apk and proceed accordingly.
+        if [[ "$f" == "$APK_NAME" ]]; then
+            display_name="$PACKAGE_NAME.apk"
+            cp "$APK_NAME" "${appDir}/$display_name"
+            [[ -e "$DESTINATION/$PACKAGE_NAME.app/$PACKAGE_NAME.apk" ]] && echo "Base apk copied" || echo "Base apk failed!"
+        else
+            display_name="$f"
+            cp "$f" "${appDir}/" && echo "Copied split apk: $f"
+        fi
+
+        # Output the file to APK_NAMES_LIST_FILE
+        if [[ -n $APK_NAMES_LIST_FILE ]]; then
+            echo "$PACKAGE_NAME:$display_name" >> $APK_NAMES_LIST_FILE
+        fi
+
+    done
 
 fi
 
