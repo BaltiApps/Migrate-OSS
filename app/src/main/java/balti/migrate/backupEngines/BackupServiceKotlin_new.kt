0aeee143bba5b67e862c0ca3272431c00750ca01
SayantanRC <sayantan_rc@protonmail.com>
2021-10-27 12:31:42 +0530

    BackupServiceKotlin_new.kt: Companion object to store destination and cancelBackup

--- app/src/main/java/balti/migrate/backupEngines/BackupServiceKotlin_new.kt
+++ app/src/main/java/balti/migrate/backupEngines/BackupServiceKotlin_new.kt
@@ -5,5 +5,18 @@ import android.content.Intent
 import android.os.IBinder
 
 class BackupServiceKotlin_new: Service() {
+
+    companion object {
+
+        var fileXDestination: String? = null
+        var cancelBackup: Boolean = false
+
+    }
+
     override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fileXDestination = null
+    }
 }
