be4b00c7fd12e043c2da98896d37758d02935a2d
SayantanRC <sayantan_rc@protonmail.com>
2021-10-27 19:35:02 +0530

    EngineJobResultHolder.kt: Add more arguments in constructor - errors and warnings.

--- app/src/main/java/balti/migrate/backupEngines/utils/EngineJobResultHolder.kt
+++ app/src/main/java/balti/migrate/backupEngines/utils/EngineJobResultHolder.kt
@@ -1,3 +1,8 @@
 package balti.migrate.backupEngines.utils
 
-data class EngineJobResultHolder(val success: Boolean, val result: Any)
+data class EngineJobResultHolder(
+    val success: Boolean,
+    val result: Any?,
+    val errors: ArrayList<String> = arrayListOf(),
+    val warnings: ArrayList<String> = arrayListOf(),
+)
