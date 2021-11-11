ab7a9fc2a9d56727928dae08ed83dacdd414653b
SayantanRC <sayantan_rc@protonmail.com>
2021-11-12 04:03:12 +0530

    [STORAGE TWEAKS] EngineJobResultHolder.kt: Define successResultNotNull variable.

--- app/src/main/java/balti/migrate/backupEngines/utils/EngineJobResultHolder.kt
+++ app/src/main/java/balti/migrate/backupEngines/utils/EngineJobResultHolder.kt
@@ -1,8 +1,17 @@
 package balti.migrate.backupEngines.utils
 
+import balti.migrate.backupEngines.BackupServiceKotlin_new
+
 data class EngineJobResultHolder(
     val success: Boolean,
     val result: Any?,
     val errors: ArrayList<String> = arrayListOf(),
     val warnings: ArrayList<String> = arrayListOf(),
-)
+){
+    /**
+     * Will be true if [success] = `true` AND [result] is not null.
+     * Used in [BackupServiceKotlin_new.startBackup].
+     * To check if result is a success and perform operations on non-null result.
+     */
+    val successResultNotNull: Boolean = success && (result != null)
+}
