d2ffdb84142b955825a4f7900783b7ce124079fe
SayantanRC <bin.sayantan@gmail.com>
2025-04-06 04:02:57 +0530

    feat(restore) - restore DI module for sms and call logs

--- app/src/main/java/balti/migrate/restore/di/RestoreDiModule.kt
+++ app/src/main/java/balti/migrate/restore/di/RestoreDiModule.kt
@@ -1,7 +1,15 @@
 package balti.migrate.restore.di
 
+import balti.migrate.common.model.CallLogData
+import balti.migrate.common.model.SmsData
+import balti.migrate.restore.data.sources.callLog.CallLogDBReader
+import balti.migrate.restore.data.sources.callLog.CallLogRestore
+import balti.migrate.restore.data.sources.sms.SmsDBReader
+import balti.migrate.restore.data.sources.sms.SmsRestore
 import balti.migrate.restore.ui.screens.BrowseRestoreDirectoryViewModel
+import baltiapps.migrate.domain.common.sources.fileSystem.DBReader
 import baltiapps.migrate.domain.restore.repository.RestoreDataRepository
+import baltiapps.migrate.domain.restore.sources.DataRestore
 import baltiapps.migrate.domain.restore.usecase.ReadCallLogForRestoreUseCase
 import baltiapps.migrate.domain.restore.usecase.ReadFilesFromBackupUseCase
 import baltiapps.migrate.domain.restore.usecase.ReadSmsForRestoreUseCase
@@ -9,17 +17,68 @@ import baltiapps.migrate.domain.restore.usecase.RestoreCallLogUseCase
 import baltiapps.migrate.domain.restore.usecase.RestoreSmsUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
+enum class Names {
+    CALL_LOG_DB_READER,
+    CALL_LOG_RESTORE_SOURCE,
+    SMS_DB_READER,
+    SMS_RESTORE_SOURCE,
+}
 
 val restoreDiModule = module {
 
-    viewModelOf(::BrowseRestoreDirectoryViewModel)
+    /* Sources */
+
+    single<DBReader<CallLogData>>(named(Names.CALL_LOG_DB_READER)) {
+        CallLogDBReader(get())
+    }
+    single<DataRestore<CallLogData>>(named(Names.CALL_LOG_RESTORE_SOURCE)) {
+        CallLogRestore(get(), get())
+    }
+    single<DBReader<SmsData>>(named(Names.SMS_DB_READER)) {
+        SmsDBReader(get())
+    }
+    single<DataRestore<SmsData>>(named(Names.SMS_RESTORE_SOURCE)) {
+        SmsRestore(get(), get())
+    }
+
+    /* Repositories */
 
     singleOf(::RestoreDataRepository)
 
-    singleOf(::ReadCallLogForRestoreUseCase)
+    /* Use cases */
+
     singleOf(::ReadFilesFromBackupUseCase)
-    singleOf(::ReadSmsForRestoreUseCase)
-    singleOf(::RestoreCallLogUseCase)
-    singleOf(::RestoreSmsUseCase)
+    single {
+        ReadCallLogForRestoreUseCase(
+            fileSystemSource = get(),
+            callLogDbReader = get(named(Names.CALL_LOG_DB_READER)),
+            restoreDataRepository = get()
+        )
+    }
+    single {
+        ReadSmsForRestoreUseCase(
+            fileSystemSource = get(),
+            smsDbReader = get(named(Names.SMS_DB_READER)),
+            restoreDataRepository = get()
+        )
+    }
+    single {
+        RestoreCallLogUseCase(
+            dataRestore = get(named(Names.CALL_LOG_RESTORE_SOURCE)),
+            restoreDataRepository = get()
+        )
+    }
+    single {
+        RestoreSmsUseCase(
+            dataRestore = get(named(Names.SMS_RESTORE_SOURCE)),
+            restoreDataRepository = get()
+        )
+    }
+
+    /* View models*/
+
+    viewModelOf(::BrowseRestoreDirectoryViewModel)
 }
