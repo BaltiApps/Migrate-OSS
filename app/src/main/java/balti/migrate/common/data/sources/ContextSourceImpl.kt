c3c821e44400ed26104d511ad3ea956e43011326
SayantanRC <bin.sayantan@gmail.com>
2025-04-10 02:27:54 +0530

    feat - context source - add SMS restore progress label

--- app/src/main/java/balti/migrate/common/data/sources/ContextSourceImpl.kt
+++ app/src/main/java/balti/migrate/common/data/sources/ContextSourceImpl.kt
@@ -15,6 +15,7 @@ class ContextSourceImpl(private val context: Context): ContextSource {
             Progress.ProgressType.BACKUP_FINISHED_WITH_ERRORS -> context.getString(R.string.backup_finished_with_errors)
             Progress.ProgressType.BACKUP_CANCELLED -> context.getString(R.string.backup_cancelled)
             Progress.ProgressType.CALL_LOG_RESTORE -> context.getString(R.string.label_call_log_restore)
+            Progress.ProgressType.SMS_RESTORE -> context.getString(R.string.label_sms_restore)
             else -> context.getString(R.string.loading)
         }
     }
