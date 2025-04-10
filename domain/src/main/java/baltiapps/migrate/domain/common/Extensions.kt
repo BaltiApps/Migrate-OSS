59790f8f3d465fc421035e2038f16eba4b77b172
SayantanRC <bin.sayantan@gmail.com>
2025-04-11 00:53:28 +0530

    feat - add runCatchingWithProgress extension function

--- domain/src/main/java/baltiapps/migrate/domain/common/Extensions.kt
+++ domain/src/main/java/baltiapps/migrate/domain/common/Extensions.kt
@@ -2,6 +2,7 @@ package baltiapps.migrate.domain.common
 
 import baltiapps.migrate.domain.common.model.DataItem
 import baltiapps.migrate.domain.common.model.ListItem
+import baltiapps.migrate.domain.common.model.Progress
 
 fun <T> MutableList<T>.clearAndAddAll(items: List<T>) {
     this.clear()
@@ -14,4 +15,23 @@ fun <T: ListItem, V: DataItem<T>> List<V>.toListItems(): List<T> {
 
 fun getPercentage(count: Number, total: Number): Double {
     return (count.toDouble() / total.toDouble())
+}
+
+fun runCatchingWithProgress(
+    progress: Progress,
+    block: () -> Unit,
+): Progress {
+    return try {
+        block()
+        progress
+    } catch (e: Exception) {
+        progress.copy(
+            logs = progress.logs.run {
+                if (isBlank()) {
+                    e.message.toString()
+                } else "$this - ${e.message}"
+            },
+            isFailure = true
+        )
+    }
 }
