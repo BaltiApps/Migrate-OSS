114795c9297bf0dea0c7f6377fc77e1cbede5a11
SayantanRC <bin.sayantan@gmail.com>
2025-04-12 03:24:06 +0530

    refactor: [WIP - REWRITE] - modified DI to add new DBWriters and usecases

--- app/src/main/java/balti/migrate/backup/di/BackupDiModule.kt
+++ app/src/main/java/balti/migrate/backup/di/BackupDiModule.kt
@@ -2,10 +2,13 @@ package balti.migrate.backup.di
 
 import balti.migrate.backup.data.sources.BackupNotificationHandlerImpl
 import balti.migrate.backup.data.sources.callLog.CallLogDBWriter
+import balti.migrate.backup.data.sources.callLog.REWRITE_CallLogDBWriter
 import balti.migrate.backup.data.sources.callLog.CallLogSource
 import balti.migrate.backup.data.sources.contacts.ContactsSource
 import balti.migrate.backup.data.sources.contacts.ContactsWriter
+import balti.migrate.backup.data.sources.contacts.REWRITE_ContactsDBWriter
 import balti.migrate.backup.data.sources.sms.SmsDBWriter
+import balti.migrate.backup.data.sources.sms.REWRITE_SmsDBWriter
 import balti.migrate.backup.data.sources.sms.SmsSource
 import balti.migrate.backup.ui.screens.listScreen.callLogBackup.CallLogBackupViewModel
 import balti.migrate.backup.ui.screens.listScreen.contactBackup.ContactBackupViewModel
@@ -20,6 +23,9 @@ import baltiapps.migrate.domain.backup.sources.DataSource
 import baltiapps.migrate.domain.backup.usecase.BackupCallLogUseCase
 import baltiapps.migrate.domain.backup.usecase.BackupContactsUseCase
 import baltiapps.migrate.domain.backup.usecase.BackupSmsUseCase
+import baltiapps.migrate.domain.backup.usecase.REWRITE_BackupCallLogUseCase
+import baltiapps.migrate.domain.backup.usecase.REWRITE_BackupContactsUseCase
+import baltiapps.migrate.domain.backup.usecase.REWRITE_BackupSmsUseCase
 import baltiapps.migrate.domain.backup.usecase.ReadCallLogUseCase
 import baltiapps.migrate.domain.backup.usecase.ReadContactsUseCase
 import baltiapps.migrate.domain.backup.usecase.ReadSmsUseCase
@@ -27,6 +33,7 @@ import baltiapps.migrate.domain.backup.usecase.StageSelectedContacts
 import baltiapps.migrate.domain.common.repository.ProgressLogRepository
 import baltiapps.migrate.domain.common.sources.NotificationHandler
 import baltiapps.migrate.domain.common.sources.fileSystem.DBWriter
+import baltiapps.migrate.domain.common.sources.fileSystem.REWRITE_DBWriter
 import baltiapps.migrate.domain.common.sources.fileSystem.TextWriter
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModel
@@ -39,8 +46,11 @@ enum class Names {
     CALL_LOG_SOURCE,
     SMS_SOURCE,
     DB_WRITER_CALL_LOG,
+    REWRITE_DB_WRITER_CALL_LOG,
     DB_WRITER_SMS,
+    REWRITE_DB_WRITER_SMS,
     CONTACTS_WRITER,
+    REWRITE_CONTACTS_WRITER,
     PROGRESS_LOG_REPOSITORY_BACKUP,
     NOTIFICATION_HANDLER_BACKUP,
 }
@@ -55,17 +65,26 @@ val backupDiModule = module {
     single<TextWriter<ContactData>>(named(Names.CONTACTS_WRITER)) {
         ContactsWriter()
     }
+    single<REWRITE_DBWriter<ContactData>>(named(Names.REWRITE_CONTACTS_WRITER)) {
+        REWRITE_ContactsDBWriter(get())
+    }
     single<DataSource<CallLogData>>(named(Names.CALL_LOG_SOURCE)) {
         CallLogSource(get(), get())
     }
     single<DBWriter<CallLogData>>(named(Names.DB_WRITER_CALL_LOG)) {
         CallLogDBWriter(get())
     }
+    single<REWRITE_DBWriter<CallLogData>>(named(Names.REWRITE_DB_WRITER_CALL_LOG)) {
+        REWRITE_CallLogDBWriter(get())
+    }
     single<DataSource<SmsData>>(named(Names.SMS_SOURCE)) {
         SmsSource(get(), get())
     }
     single<DBWriter<SmsData>>(named(Names.DB_WRITER_SMS)) {
         SmsDBWriter(get())
+    }
+    single<REWRITE_DBWriter<SmsData>>(named(Names.REWRITE_DB_WRITER_SMS)) {
+        REWRITE_SmsDBWriter(get())
     }
     single<NotificationHandler<*>>(named(Names.NOTIFICATION_HANDLER_BACKUP)) {
         BackupNotificationHandlerImpl(
@@ -119,6 +138,27 @@ val backupDiModule = module {
         BackupSmsUseCase(
             fileSystemSource = get(),
             smsDBWriter = get(named(Names.DB_WRITER_SMS))
+        )
+    }
+    single {
+        REWRITE_BackupContactsUseCase(
+            fileSystemSource = get(),
+            contactsDBWriter = get(named(Names.REWRITE_CONTACTS_WRITER)),
+            dataRepository = get()
+        )
+    }
+    single {
+        REWRITE_BackupCallLogUseCase(
+            fileSystemSource = get(),
+            callLogDBWriter = get(named(Names.REWRITE_DB_WRITER_CALL_LOG)),
+            dataRepository = get()
+        )
+    }
+    single {
+        REWRITE_BackupSmsUseCase(
+            fileSystemSource = get(),
+            smsDBWriter = get(named(Names.REWRITE_DB_WRITER_SMS)),
+            dataRepository = get()
         )
     }
 
