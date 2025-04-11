25e5c3f556e0bad0a9c79e084f88a2f1a1ee66fc
SayantanRC <bin.sayantan@gmail.com>
2025-04-12 03:22:46 +0530

    refactor: [WIP - REWRITE] - new usecases using new DBWriters for backing up contacts, call logs, sms

---
+++ domain/src/main/java/baltiapps/migrate/domain/backup/usecase/REWRITE_BackupCallLogUseCase.kt
@@ -0,0 +1,31 @@
+package baltiapps.migrate.domain.backup.usecase
+
+import baltiapps.migrate.domain.backup.repository.BackupDataRepository
+import baltiapps.migrate.domain.common.model.CallLogListItem
+import baltiapps.migrate.domain.common.model.DataItem
+import baltiapps.migrate.domain.common.model.Directory
+import baltiapps.migrate.domain.common.model.GenericFile
+import baltiapps.migrate.domain.common.model.Progress
+import baltiapps.migrate.domain.common.sources.fileSystem.REWRITE_DBWriter
+import baltiapps.migrate.domain.common.sources.fileSystem.FileSystemSource
+import kotlinx.coroutines.flow.Flow
+
+class REWRITE_BackupCallLogUseCase(
+    private val fileSystemSource: FileSystemSource,
+    private val callLogDBWriter: REWRITE_DBWriter<DataItem<CallLogListItem>>,
+    private val dataRepository: BackupDataRepository,
+) {
+    operator fun invoke(
+        directory: Directory,
+        file: GenericFile,
+    ): Flow<Progress> {
+        return fileSystemSource.writeDB(
+            directory = directory,
+            file = file,
+            dbWriter = callLogDBWriter,
+            writerBlock = {
+                it.writeRows(dataRepository.stagedCallLogs)
+            }
+        )
+    }
+}
---
+++ domain/src/main/java/baltiapps/migrate/domain/backup/usecase/REWRITE_BackupSmsUseCase.kt
@@ -0,0 +1,31 @@
+package baltiapps.migrate.domain.backup.usecase
+
+import baltiapps.migrate.domain.backup.repository.BackupDataRepository
+import baltiapps.migrate.domain.common.model.DataItem
+import baltiapps.migrate.domain.common.model.Directory
+import baltiapps.migrate.domain.common.model.GenericFile
+import baltiapps.migrate.domain.common.model.Progress
+import baltiapps.migrate.domain.common.model.SmsListItem
+import baltiapps.migrate.domain.common.sources.fileSystem.REWRITE_DBWriter
+import baltiapps.migrate.domain.common.sources.fileSystem.FileSystemSource
+import kotlinx.coroutines.flow.Flow
+
+class REWRITE_BackupSmsUseCase(
+    private val fileSystemSource: FileSystemSource,
+    private val smsDBWriter: REWRITE_DBWriter<DataItem<SmsListItem>>,
+    private val dataRepository: BackupDataRepository,
+) {
+    operator fun invoke(
+        directory: Directory,
+        file: GenericFile,
+    ): Flow<Progress> {
+        return fileSystemSource.writeDB(
+            directory = directory,
+            file = file,
+            dbWriter = smsDBWriter,
+            writerBlock = {
+                it.writeRows(dataRepository.stagedSms)
+            }
+        )
+    }
+}
