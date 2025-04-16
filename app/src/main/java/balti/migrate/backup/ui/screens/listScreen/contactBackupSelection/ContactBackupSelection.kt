cd2689bd14c1a4c0f94eba6c5e77314c80264d70
SayantanRC <bin.sayantan@gmail.com>
2025-04-16 13:07:15 +0530

    feat - ContactBackupSelection - show separate warning screen for the case where all contacts are synced contacts

--- app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/ContactBackupSelection.kt
+++ app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/ContactBackupSelection.kt
@@ -1,13 +1,18 @@
 package balti.migrate.backup.ui.screens.listScreen.contactBackupSelection
 
 import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
@@ -15,6 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -85,62 +92,181 @@ private fun Content(
     onItemToggled: (item: ContactListItem) -> Unit,
     onNext: () -> Unit,
 ) {
+    val syncedContactsExpanded = state().syncedContactsExpanded
+    val localContactsExpanded = state().localContactsExpanded
+    val syncedContactList = state().syncedContacts
+    val localContactList = state().localContacts
+    val shouldShowOnlySyncedContacts = localContactList.isEmpty() && syncedContactList.isNotEmpty()
+    val shouldShowOnlyLocalContacts = syncedContactList.isEmpty() && localContactList.isNotEmpty()
+    val shouldShowNoData = syncedContactList.isEmpty() && localContactList.isEmpty()
+    val nextButtonCustomLabel = stringResource(R.string.skip).takeIf {
+        shouldShowOnlySyncedContacts && !syncedContactsExpanded
+    }
+    val enableMultiSelectButtons =
+        (localContactsExpanded && localContactList.isNotEmpty()) ||
+                (syncedContactsExpanded && syncedContactList.isNotEmpty())
     val listState = ListState(
         listTitle = stringResource(R.string.label_contacts_backup),
         isStaging = state().isStaging,
         hasPermission = state().hasPermission,
         permissionDescription = stringResource(R.string.contacts_backup_permission_description),
         progress = state().progress,
-        hasNoData = state().contactList.isEmpty(),
+        hasNoData = shouldShowNoData,
     )
     ListScreenShell(
         listState = listState,
         navigateUp = navigateUp,
-        onSelectAll = onSelectAll,
-        onDeselectAll = onDeselectAll,
+        onSelectAll = onSelectAll.takeIf { enableMultiSelectButtons },
+        onDeselectAll = onDeselectAll.takeIf { enableMultiSelectButtons },
         onPermissionRequest = requestPermission,
         onNext = onNext,
+        nextButtonCustomLabel = nextButtonCustomLabel,
     ) {
-        val syncedContactsExpanded = state().syncedContactsExpanded
-        val localContactsExpanded = state().localContactsExpanded
+        when {
+            shouldShowOnlySyncedContacts -> OnlySyncedContacts(
+                syncedContactList = syncedContactList,
+                isStaging = state().isStaging,
+                shouldShowContacts = syncedContactsExpanded,
+                onShowContactsConfirmation = {
+                    onToggleSyncedContactsVisibility(true)
+                },
+                onItemToggled = onItemToggled,
+            )
+            shouldShowOnlyLocalContacts -> OnlyLocalContacts(
+                localContactList = localContactList,
+                isStaging = state().isStaging,
+                onItemToggled = onItemToggled,
+            )
+            else -> LocalAndSyncedContacts(
+                syncedContactList = syncedContactList,
+                localContactList = localContactList,
+                isStaging = state().isStaging,
+                syncedContactsExpanded = syncedContactsExpanded,
+                localContactsExpanded = localContactsExpanded,
+                onToggleSyncedContactsVisibility = onToggleSyncedContactsVisibility,
+                onToggleLocalContactsVisibility = onToggleLocalContactsVisibility,
+                onItemToggled = onItemToggled,
+            )
+        }
+    }
+}
+
+@Composable
+private fun OnlySyncedContacts(
+    syncedContactList: List<ContactListItem>,
+    isStaging: Boolean,
+    shouldShowContacts: Boolean,
+    onShowContactsConfirmation: () -> Unit,
+    onItemToggled: (item: ContactListItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    if (shouldShowContacts) {
         LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = modifier
+                .padding(8.dp)
+                .fillMaxSize()
         ) {
-            item {
-                ContactHeader(
-                    header = stringResource(R.string.synced_contacts),
-                    isExpanded = syncedContactsExpanded,
-                    onClick = {
-                        onToggleSyncedContactsVisibility(!syncedContactsExpanded)
-                    },
-                )
+            showContactList(
+                contactList = syncedContactList,
+                isStaging = isStaging,
+                onItemToggled = onItemToggled,
+            )
+        }
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = stringResource(R.string.all_contacts_are_synced_contacts),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.size(12.dp))
+            Text(
+                text = stringResource(R.string.synced_contacts_backup_is_not_recommended),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.size(12.dp))
+            OutlinedButton(
+                onClick = onShowContactsConfirmation,
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
+            ) {
+                Text(stringResource(R.string.show_synced_contacts_anyway))
             }
-            if (syncedContactsExpanded) {
-                showContactList(
-                    contactList = state().syncedContacts,
-                    isStaging = listState.isStaging,
-                    onItemToggled = onItemToggled,
-                )
-            }
-            item {
-                Spacer(modifier = Modifier.padding(4.dp))
-            }
-            item {
-                ContactHeader(
-                    header = stringResource(R.string.local_contacts),
-                    isExpanded = localContactsExpanded,
-                    onClick = {
-                        onToggleLocalContactsVisibility(!localContactsExpanded)
-                    }
-                )
-            }
-            if (localContactsExpanded) {
-                showContactList(
-                    contactList = state().localContacts,
-                    isStaging = listState.isStaging,
-                    onItemToggled = onItemToggled,
-                )
-            }
+        }
+    }
+}
+
+@Composable
+private fun OnlyLocalContacts(
+    localContactList: List<ContactListItem>,
+    isStaging: Boolean,
+    onItemToggled: (item: ContactListItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        showContactList(
+            contactList = localContactList,
+            isStaging = isStaging,
+            onItemToggled = onItemToggled,
+        )
+    }
+}
+
+@Composable
+private fun LocalAndSyncedContacts(
+    syncedContactList: List<ContactListItem>,
+    localContactList: List<ContactListItem>,
+    isStaging: Boolean,
+    syncedContactsExpanded: Boolean,
+    localContactsExpanded: Boolean,
+    onToggleSyncedContactsVisibility: (isVisible: Boolean) -> Unit,
+    onToggleLocalContactsVisibility: (isVisible: Boolean) -> Unit,
+    onItemToggled: (item: ContactListItem) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        item {
+            ContactHeader(
+                header = stringResource(R.string.synced_contacts),
+                isExpanded = syncedContactsExpanded,
+                onClick = {
+                    onToggleSyncedContactsVisibility(!syncedContactsExpanded)
+                },
+            )
+        }
+        if (syncedContactsExpanded) {
+            showContactList(
+                contactList = syncedContactList,
+                isStaging = isStaging,
+                onItemToggled = onItemToggled,
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+        item {
+            ContactHeader(
+                header = stringResource(R.string.local_contacts),
+                isExpanded = localContactsExpanded,
+                onClick = {
+                    onToggleLocalContactsVisibility(!localContactsExpanded)
+                }
+            )
+        }
+        if (localContactsExpanded) {
+            showContactList(
+                contactList = localContactList,
+                isStaging = isStaging,
+                onItemToggled = onItemToggled,
+            )
         }
     }
 }
