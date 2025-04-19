506b9969c7c0b25e55a7a71517be76f08b1d86ff
SayantanRC <bin.sayantan@gmail.com>
2025-04-19 23:10:53 +0530

    feat - create use case to export contacts for restore

---
+++ domain/src/main/java/baltiapps/migrate/domain/restore/usecase/ExportContactsForRestoreUseCase.kt
@@ -0,0 +1,41 @@
+package baltiapps.migrate.domain.restore.usecase
+
+import baltiapps.migrate.domain.common.getPercentage
+import baltiapps.migrate.domain.common.model.ContactListItem
+import baltiapps.migrate.domain.common.model.DataItem
+import baltiapps.migrate.domain.common.model.GenericFile
+import baltiapps.migrate.domain.common.model.Progress
+import baltiapps.migrate.domain.common.runCatchingWithProgress
+import baltiapps.migrate.domain.common.sources.fileSystem.TextWriter
+import baltiapps.migrate.domain.restore.repository.RestoreDataRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class ExportContactsForRestoreUseCase(
+    private val dataRestore: RestoreDataRepository,
+) {
+    operator fun invoke(
+        file: GenericFile,
+        textWriter: TextWriter<String>,
+        getContactContent: (item: DataItem<ContactListItem>) -> String,
+    ): Flow<Progress> {
+        return flow {
+            if (!dataRestore.shouldRestoreContacts()) return@flow
+            val stagedContacts = dataRestore.stagedContacts
+            textWriter.setup(file.path, false)
+            stagedContacts.forEachIndexed { index, item ->
+                val progress = Progress(
+                    progressType = Progress.ProgressType.CONTACTS_EXPORT,
+                    percentage = getPercentage(index + 1, stagedContacts.size),
+                    logs = "(${index + 1}/${stagedContacts.size}) ${item.logInfo}"
+                )
+                runCatchingWithProgress(progress) {
+                    textWriter.writeLine(getContactContent(item))
+                }.run { emit(this) }
+            }
+            textWriter.close()
+        }.flowOn(Dispatchers.IO)
+    }
+}
