1362203f35f7e5d16edd9a614965f155eeaae040
SayantanRC <bin.sayantan@gmail.com>
2025-04-19 23:09:41 +0530

    feat: create EmptyImageVector

---
+++ app/src/main/java/balti/migrate/common/ui/components/EmptyImageVector.kt
@@ -0,0 +1,12 @@
+package balti.migrate.common.ui.components
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+val EmptyImageVector = ImageVector.Builder(
+    name = "EmptyImageVector",
+    defaultWidth = 0.dp,
+    defaultHeight = 0.dp,
+    viewportWidth = 0f,
+    viewportHeight = 0f,
+).build()
