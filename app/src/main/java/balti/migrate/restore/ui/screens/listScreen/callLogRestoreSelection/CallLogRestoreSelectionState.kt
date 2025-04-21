8a28ba3e5561dc7d6f6b7b9dc6dbc696ec9231b5
SayantanRC <bin.sayantan@gmail.com>
2025-04-21 17:38:00 +0530

    fix - initially set hasPermission to true in all list screen states to avoid flashing the no permission dialog

--- app/src/main/java/balti/migrate/restore/ui/screens/listScreen/callLogRestoreSelection/CallLogRestoreSelectionState.kt
+++ app/src/main/java/balti/migrate/restore/ui/screens/listScreen/callLogRestoreSelection/CallLogRestoreSelectionState.kt
@@ -7,5 +7,5 @@ data class CallLogRestoreSelectionState(
     val progress: Progress = Progress.Empty,
     val callLogList: List<CallLogListItem> = emptyList(),
     val isStaging: Boolean = false,
-    val hasPermission: Boolean = false,
+    val hasPermission: Boolean = true,
 )
