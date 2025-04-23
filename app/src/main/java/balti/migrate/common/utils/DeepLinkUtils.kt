47ecfc1a5b052e435397a08d21cadd23e7394633
SayantanRC <bin.sayantan@gmail.com>
2025-04-23 10:55:34 +0530

    feat - create DeepLinkUtils

---
+++ app/src/main/java/balti/migrate/common/utils/DeepLinkUtils.kt
@@ -0,0 +1,35 @@
+package balti.migrate.common.utils
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.net.toUri
+import balti.migrate.BuildConfig
+import balti.migrate.MainActivity
+
+class DeepLinkUtils {
+
+    enum class MigrateUri(val uriString: String) {
+        UriProgressBackup("${BuildConfig.SCHEMA}://${BuildConfig.HOST_PROGRESS_BACKUP}"),
+        UriProgressRestore("${BuildConfig.SCHEMA}://${BuildConfig.HOST_PROGRESS_RESTORE}"),
+        ;
+
+        val uri: Uri get() = uriString.toUri()
+    }
+
+    fun getPendingIntent(uri: MigrateUri, context: Context): PendingIntent {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            uri.uri,
+            context,
+            MainActivity::class.java
+        )
+        return PendingIntent.getActivity(
+            context,
+            uri.ordinal + 1,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+}
