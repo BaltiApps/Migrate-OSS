55c02059005b8bf06a4157b4ea7d448c5a9681fe
SayantanRC <bin.sayantan@gmail.com>
2025-04-28 12:30:52 +0530

    feat - add About dialog and info icon on home screen

--- app/src/main/java/balti/migrate/ui/screens/ScreenHome.kt
+++ app/src/main/java/balti/migrate/ui/screens/ScreenHome.kt
@@ -5,11 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -22,8 +32,26 @@ fun ScreenHome(
     onBackupSelected: () -> Unit,
     onRestoreSelected: () -> Unit
 ) {
+
+    var shouldShowAboutDialog by remember { mutableStateOf(false) }
+
+    if (shouldShowAboutDialog) {
+        AboutDialog(
+            onDismissRequest = {
+                shouldShowAboutDialog = false
+            }
+        )
+    }
+
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopBar(
+                showAboutDialog = {
+                    shouldShowAboutDialog = true
+                }
+            )
+        }
     ) { values ->
         Column(
             modifier = Modifier
@@ -44,6 +72,28 @@ fun ScreenHome(
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar(
+    showAboutDialog: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = {},
+        actions = {
+            IconButton(
+                onClick = showAboutDialog
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Info,
+                    contentDescription = stringResource(R.string.about),
+                )
+            }
+        },
+        modifier = modifier,
+    )
 }
 
 @Preview
