31a1f8abb40c08adab25a02ba7e9692b6ca5c6cd
SayantanRC <bin.sayantan@gmail.com>
2025-04-28 11:18:38 +0530

    build - update compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "balti.migrate"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "balti.migrate"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
