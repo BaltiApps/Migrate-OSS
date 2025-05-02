5dccaa1923c40d178e6a8433773c02f2b8239e71
SayantanRC <bin.sayantan@gmail.com>
2025-05-02 16:21:08 +0530

    [WIP] feat: add MediaStoreDownloadFile class extending GenericFile

---
+++ app/src/main/java/balti/migrate/common/data/model/MediaStoreDownloadFile.kt
@@ -0,0 +1,8 @@
+package balti.migrate.common.data.model
+
+import baltiapps.migrate.domain.common.model.GenericFile
+
+class MediaStoreDownloadFile(
+    override val path: String,
+) : GenericFile {
+}
