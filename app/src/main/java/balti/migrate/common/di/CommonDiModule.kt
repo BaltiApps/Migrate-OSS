c76d6f20d9b76910a45b67802ff1dadaa3c5de99
SayantanRC <bin.sayantan@gmail.com>
2025-05-04 20:11:16 +0530

    [WIP] remove Directory, DirectoryBrowser, DirectoryBrowserImpl

--- app/src/main/java/balti/migrate/common/di/CommonDiModule.kt
+++ app/src/main/java/balti/migrate/common/di/CommonDiModule.kt
@@ -2,14 +2,12 @@ package balti.migrate.common.di
 
 import balti.migrate.common.data.sources.ContextSourceImpl
 import balti.migrate.common.data.sources.PreferencesImpl
-import balti.migrate.common.data.sources.fileSystem.DirectoryBrowserImpl
 import balti.migrate.common.data.sources.fileSystem.FileSystemSourceImpl
 import balti.migrate.common.data.sources.fileSystem.TextWriterImpl
 import balti.migrate.common.utils.DBUtils
 import balti.migrate.common.utils.ListItemUtils
 import baltiapps.migrate.domain.common.sources.ContextSource
 import baltiapps.migrate.domain.common.sources.Preferences
-import baltiapps.migrate.domain.common.sources.fileSystem.DirectoryBrowser
 import baltiapps.migrate.domain.common.sources.fileSystem.FileSystemSource
 import baltiapps.migrate.domain.common.sources.fileSystem.TextWriter
 import baltiapps.migrate.domain.common.usecase.StageSelectedCallLogs
@@ -38,8 +36,6 @@ val commonDiModule = module {
     }
 
     singleOf(::FileSystemSourceImpl) bind FileSystemSource::class
-
-    singleOf(::DirectoryBrowserImpl) bind DirectoryBrowser::class
 
     singleOf(::ContextSourceImpl) bind ContextSource::class
 
