41198f9027b6997ce2927a0ad5b5c167793591c4
SayantanRC <bin.sayantan@gmail.com>
2025-05-26 18:42:12 +0530

    feat - show selected count for synced and local contacts during contact backup

--- app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/ContactComponents.kt
+++ app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/ContactComponents.kt
@@ -3,6 +3,8 @@ package balti.migrate.backup.ui.screens.listScreen.contactBackupSelection
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,7 +14,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,12 +21,13 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import balti.migrate.R
+import balti.migrate.common.ui.components.CountBar
 import balti.migrate.common.ui.components.RenderContactItem
 import baltiapps.migrate.domain.common.model.ContactListItem
 
 @Composable
 fun ContactHeader(
-    header: String,
+    header: @Composable ColumnScope.() -> Unit,
     isExpanded: Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -38,10 +40,9 @@ fun ContactHeader(
             .padding(horizontal = 16.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Text(
-            text = header,
+        Column(
             modifier = Modifier.weight(1f),
-            style = MaterialTheme.typography.labelLarge,
+            content = header
         )
         Image(
             imageVector = if (isExpanded) {
@@ -59,8 +60,18 @@ fun LazyListScope.showContactList(
     contactList: List<ContactListItem>,
     isStaging: Boolean,
     onItemToggled: (item: ContactListItem) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    shouldShowCountBar: Boolean = true,
 ) {
+    if (shouldShowCountBar) {
+        stickyHeader {
+            val selectedItems = contactList.filter { it.isChecked }
+            CountBar(
+                totalCount = contactList.size,
+                selectedCount = selectedItems.size,
+            )
+        }
+    }
     items(
         items = contactList,
         key = { it._id }
--- app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/LocalAndSyncedContacts.kt
+++ app/src/main/java/balti/migrate/backup/ui/screens/listScreen/contactBackupSelection/LocalAndSyncedContacts.kt
@@ -4,9 +4,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import balti.migrate.R
 import baltiapps.migrate.domain.common.model.ContactListItem
@@ -24,12 +31,27 @@ fun LocalAndSyncedContacts(
     onItemToggled: (item: ContactListItem) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val syncedContactCount = syncedContactList.size
+    val localContactCount = localContactList.size
+    val selectedSyncedContactCount = syncedContactList.filter { it.isChecked }.size
+    val selectedLocalContactCount = localContactList.filter { it.isChecked }.size
+    val syncedCountString = "$selectedSyncedContactCount / $syncedContactCount"
+    val localCountString = "$selectedLocalContactCount / $localContactCount"
     LazyColumn(
         modifier = modifier.fillMaxSize(),
     ) {
-        item {
+        stickyHeader {
             ContactHeader(
-                header = stringResource(R.string.synced_contacts),
+                header = {
+                    Text(
+                        text = stringResource(R.string.synced_contacts),
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+                    Text(
+                        text = "${stringResource(R.string.selected_items)} - $syncedCountString",
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+                },
                 isExpanded = syncedContactsExpanded,
                 onClick = {
                     onToggleSyncedContactsVisibility(!syncedContactsExpanded)
@@ -41,14 +63,24 @@ fun LocalAndSyncedContacts(
                 contactList = syncedContactList,
                 isStaging = isStaging,
                 onItemToggled = onItemToggled,
+                shouldShowCountBar = false,
             )
         }
         item {
             Spacer(modifier = Modifier.padding(4.dp))
         }
-        item {
+        stickyHeader {
             ContactHeader(
-                header = stringResource(R.string.local_contacts),
+                header = {
+                    Text(
+                        text = stringResource(R.string.local_contacts),
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+                    Text(
+                        text = "${stringResource(R.string.selected_items)} - $localCountString",
+                        style = MaterialTheme.typography.labelLarge,
+                    )
+                },
                 isExpanded = localContactsExpanded,
                 onClick = {
                     onToggleLocalContactsVisibility(!localContactsExpanded)
@@ -60,7 +92,119 @@ fun LocalAndSyncedContacts(
                 contactList = localContactList,
                 isStaging = isStaging,
                 onItemToggled = onItemToggled,
+                shouldShowCountBar = false,
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun ContactPreview() {
+    val syncedContacts = listOf<ContactListItem>(
+        ContactListItem(
+            _id = "1",
+            displayName = "s name1",
+            isLocalContact = false,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "2",
+            displayName = "s name2",
+            isLocalContact = false,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "3",
+            displayName = "s name3",
+            isLocalContact = false,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "4",
+            displayName = "s name4",
+            isLocalContact = false,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "5",
+            displayName = "s name5",
+            isLocalContact = false,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "6",
+            displayName = "s name6",
+            isLocalContact = false,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "7",
+            displayName = "s name7",
+            isLocalContact = false,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "8",
+            displayName = "s name8",
+            isLocalContact = false,
+            isChecked = true,
+        )
+    )
+    val localContacts = listOf<ContactListItem>(
+        ContactListItem(
+            _id = "9",
+            displayName = "l name1",
+            isLocalContact = true,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "10",
+            displayName = "l name2",
+            isLocalContact = true,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "11",
+            displayName = "l name3",
+            isLocalContact = true,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "12",
+            displayName = "l name4",
+            isLocalContact = true,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "13",
+            displayName = "l name5",
+            isLocalContact = true,
+            isChecked = false,
+        ),
+        ContactListItem(
+            _id = "14",
+            displayName = "l name6",
+            isLocalContact = true,
+            isChecked = true,
+        ),
+        ContactListItem(
+            _id = "15",
+            displayName = "l name7",
+            isLocalContact = true,
+            isChecked = false,
+        )
+    )
+    var syncedContactsExpanded by remember { mutableStateOf(false) }
+    var localContactsExpanded by remember { mutableStateOf(true) }
+    LocalAndSyncedContacts(
+        syncedContactList = syncedContacts,
+        localContactList = localContacts,
+        isStaging = false,
+        syncedContactsExpanded = syncedContactsExpanded,
+        localContactsExpanded = localContactsExpanded,
+        onToggleSyncedContactsVisibility = { syncedContactsExpanded = it },
+        onToggleLocalContactsVisibility = { localContactsExpanded = it },
+        onItemToggled = {},
+    )
 }
