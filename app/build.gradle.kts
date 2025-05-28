26c65cd7cf374d8921157641d55a9089ff94ee76
SayantanRC <bin.sayantan@gmail.com>
2025-05-28 22:07:52 +0530

    Revert "feat - dark mode - use AppCompatActivity to fix status bar icon colors"
    This reverts commit bf3ff05da2583cbf42befc63f7da5ada01ff931f.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,6 @@ dependencies {
     implementation(libs.com.jakewharton.timber)
     implementation(libs.com.google.guava)
     implementation(libs.androidx.documentfile)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/balti/migrate/app/MainActivity.kt
+++ app/src/main/java/balti/migrate/app/MainActivity.kt
@@ -2,10 +2,9 @@ package balti.migrate.app
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.isSystemInDarkTheme
 import balti.migrate.app.ui.navigation.Graph
 import balti.migrate.app.ui.theme.MigrateTheme
@@ -21,13 +20,12 @@ import baltiapps.migrate.domain.backup.model.BackupLocation
 import baltiapps.migrate.domain.common.sources.Preferences.DarkMode
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private val viewModel: MainActivityViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setAppDarkMode(viewModel.darkMode.value)
         enableEdgeToEdge()
         setContent {
             MigrateTheme(
@@ -44,24 +42,10 @@ class MainActivity : AppCompatActivity() {
                     startRestoreService = ::startRestoreService,
                     cancelRestore = ::cancelRestore,
                     shouldShowPermissionScreen = viewModel::shouldShowPermissionScreen,
-                    updateUiState = { darkMode, followSystemColors ->
-                        setAppDarkMode(darkMode)
-                        viewModel.updateUiState(darkMode, followSystemColors)
-                    },
+                    updateUiState = viewModel::updateUiState,
                 )
             }
         }
-    }
-
-    private fun setAppDarkMode(darkMode: DarkMode) {
-        println(darkMode)
-        AppCompatDelegate.setDefaultNightMode(
-            when(darkMode) {
-                DarkMode.LIGHT -> AppCompatDelegate.MODE_NIGHT_NO
-                DarkMode.DARK -> AppCompatDelegate.MODE_NIGHT_YES
-                DarkMode.SYSTEM -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-            }
-        )
     }
 
     private fun startBackupService(backupLocation: BackupLocation) {
