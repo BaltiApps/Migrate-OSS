0a92fc176d3e804446a40a41119cfb1bedf3c121
SayantanRC <bin.sayantan@gmail.com>
2025-09-01 00:34:29 +0530

    feat: RenderAppListItem - create composable to render app list item

---
+++ app/src/main/java/balti/migrate/common/ui/components/RenderAppListItem.kt
@@ -0,0 +1,149 @@
+package balti.migrate.common.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import balti.migrate.R
+import balti.migrate.common.data.model.AppIcon
+import baltiapps.migrate.domain.common.model.AppListItem
+
+@Composable
+fun RenderAppListItem(
+    item: AppListItem,
+    enabled: Boolean,
+    onApkSelected: (item: AppListItem) -> Unit,
+    onDataSelected: (item: AppListItem) -> Unit,
+    onPermissionSelected: (item: AppListItem) -> Unit,
+    onItemClicked: (AppListItem) -> Unit,
+) {
+    val alphaModifier = Modifier.alpha(
+        if (enabled) 1f else 0.38f
+    )
+
+    Row(
+        modifier = Modifier
+            .clickable(enabled = enabled) {
+                onItemClicked(item)
+            }
+            .padding(dimensionResource(R.dimen.column_item_padding)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        AppListAppIcon(
+            item = item,
+            modifier = alphaModifier.size(48.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp),
+        ) {
+            Text(
+                text = item.appName,
+                modifier = alphaModifier,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                modifier = Modifier.basicMarquee(),
+                text = item._id,
+                fontWeight = FontWeight.Thin,
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                checked = item.isApkSelected,
+                onCheckedChange = {
+                    onApkSelected(item)
+                },
+                enabled = enabled,
+            )
+            Checkbox(
+                checked = item.isDataSelected,
+                onCheckedChange = {
+                    onDataSelected(item)
+                },
+                enabled = enabled,
+            )
+            Checkbox(
+                checked = item.isPermissionsSelected,
+                onCheckedChange = {
+                    onPermissionSelected(item)
+                },
+                enabled = enabled,
+            )
+        }
+    }
+}
+
+@Composable
+private fun AppListAppIcon(
+    item: AppListItem,
+    modifier: Modifier = Modifier,
+) {
+    (item.appIcon as? AppIcon)?.run {
+        val bitmap = this.drawable.toBitmap().asImageBitmap()
+        Image(
+            modifier = modifier,
+            bitmap = bitmap,
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RenderAppListItemPreview() {
+    val context = LocalContext.current
+
+    val item = AppListItem(
+        _id = "com.example.app",
+        appName = "Example App",
+
+        appIcon = AppIcon(
+            drawable = ContextCompat.getDrawable(context, R.mipmap.ic_launcher)!!
+        ),
+
+        versionName = "1.0.0",
+        versionCode = 1,
+
+        isApkSelected = true,
+        isDataSelected = false,
+        isPermissionsSelected = true,
+    )
+
+    RenderAppListItem(
+        item = item,
+        enabled = true,
+        onApkSelected = {},
+        onDataSelected = {},
+        onPermissionSelected = {},
+        onItemClicked = {},
+    )
+}
