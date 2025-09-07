72376fb74b255af239f1540f330449619f687179
SayantanRC <bin.sayantan@gmail.com>
2025-09-07 17:57:07 +0530

    feat(domain): implement `BackupAppsInfoUseCase` to use AppIconWriter and AppInfoWriter

---
+++ domain/src/main/java/baltiapps/migrate/domain/backup/usecase/BackupAppsInfoUseCase.kt
@@ -0,0 +1,48 @@
+package baltiapps.migrate.domain.backup.usecase
+
+import baltiapps.migrate.domain.backup.repository.BackupDataRepository
+import baltiapps.migrate.domain.common.getPercentage
+import baltiapps.migrate.domain.common.model.AppListItem
+import baltiapps.migrate.domain.common.model.DataItem
+import baltiapps.migrate.domain.common.model.Progress
+import baltiapps.migrate.domain.common.sources.fileSystem.TextWriter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class BackupAppsInfoUseCase(
+    private val appIconWriter: TextWriter<DataItem<AppListItem>>,
+    private val appInfoWriter: TextWriter<DataItem<AppListItem>>,
+    private val dataRepository: BackupDataRepository,
+) {
+    operator fun invoke(
+        internalBackupPath: String,
+    ): Flow<Progress> {
+        return flow {
+            dataRepository.stagedApps.forEachIndexed { index, appListItem ->
+
+                appInfoWriter.setup(
+                    fileLocation = internalBackupPath,
+                    fileName = "${appListItem._id}.json",
+                    append = false
+                )
+                appInfoWriter.write(appListItem)
+                appIconWriter.setup(
+                    fileLocation = internalBackupPath,
+                    fileName = "${appListItem._id}.mpng",
+                    append = false
+                )
+                appIconWriter.write(appListItem)
+
+                val appName = appListItem.logInfo.substringBeforeLast(':').trim()
+
+                emit(Progress(
+                    logs = appName,
+                    progressType = Progress.ProgressType.APP_INFO_BACKUP,
+                    percentage = getPercentage(index+1, dataRepository.stagedApps.size),
+                ))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+}
