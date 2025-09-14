19003e1d45fae66c4a5073ccd35b3c1ae3797194
SayantanRC <bin.sayantan@gmail.com>
2025-09-15 01:25:34 +0530

    feat: add default transparent drawable to AppIcon

--- app/src/main/java/balti/migrate/common/data/model/AppIcon.kt
+++ app/src/main/java/balti/migrate/common/data/model/AppIcon.kt
@@ -1,8 +1,10 @@
 package balti.migrate.common.data.model
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.drawable.toDrawable
 import baltiapps.migrate.domain.common.model.DrawableAsset
 
 data class AppIcon(
-    val drawable: Drawable
+    val drawable: Drawable = Color.TRANSPARENT.toDrawable(),
 ): DrawableAsset
