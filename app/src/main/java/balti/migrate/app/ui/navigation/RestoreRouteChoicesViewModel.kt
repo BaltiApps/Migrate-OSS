396eaa3071cfe2357830ea67d66f2bd9216cf945
SayantanRC <bin.sayantan@gmail.com>
2025-09-18 01:51:40 +0530

    feat: remove superuser check from RestoreRouteChoicesViewModel

--- app/src/main/java/balti/migrate/app/ui/navigation/RestoreRouteChoicesViewModel.kt
+++ app/src/main/java/balti/migrate/app/ui/navigation/RestoreRouteChoicesViewModel.kt
@@ -1,14 +1,9 @@
 package balti.migrate.app.ui.navigation
 
 import androidx.lifecycle.ViewModel
-import balti.migrate.common.utils.SuperuserUtils
-import baltiapps.migrate.domain.common.sources.Preferences
 import baltiapps.migrate.domain.restore.repository.RestoreDataRepository
-import kotlinx.coroutines.runBlocking
 
 class RestoreRouteChoicesViewModel(
-    private val preferences: Preferences,
-    private val superuserUtils: SuperuserUtils,
     private val dataRepository: RestoreDataRepository
 ) : ViewModel() {
 
@@ -16,21 +11,11 @@ class RestoreRouteChoicesViewModel(
         RouteCallLogRestoreSelection to { dataRepository.getCallLogBackupFile() != null },
         RouteSmsRestoreSelection to { dataRepository.getSmsBackupFile() != null },
         RouteContactRestoreSelection to { dataRepository.getContactBackupFile() != null },
-        RouteAppRestoreSelection to {
-            dataRepository.getAppInfoFiles().isNotEmpty() &&
-                    preferences.wasSuperuserPermissionPreviouslyGranted() &&
-                    checkSuperuserPermission()
-        },
+        RouteAppRestoreSelection to { dataRepository.getAppInfoFiles().isNotEmpty() },
         RouteRestoreSummary to { true },
     )
 
     private var pointerIndex = -1
-
-    private fun checkSuperuserPermission(): Boolean {
-        return runBlocking {
-            superuserUtils.checkSuperuserPermission().isSuccess
-        }
-    }
 
     fun findNextRoute(): Any {
         while (pointerIndex < routeMap.size) {
