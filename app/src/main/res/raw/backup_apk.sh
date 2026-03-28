22d59f708ed0fe9f25d7025ac1f5410fa64241be
SayantanRC <bin.sayantan@gmail.com>
2026-03-28 17:31:24 +0530

    refactor [AI]: backup_apk, backup_data - change tabs from 4 spaces to 2 spaces

--- app/src/main/res/raw/backup_apk.sh
+++ app/src/main/res/raw/backup_apk.sh
@@ -13,31 +13,31 @@ NULL_MARKER="$5"
 END_MARKER="$6"
 
 print_end_marker() {
-    echo "$END_MARKER"
-    echo "$END_MARKER" >&2
+  echo "$END_MARKER"
+  echo "$END_MARKER" >&2
 }
 
 echo "=== $APP_NAME (APK) ==="
 
 if [ ! -e "$DESTINATION" ]; then
-    echo "Destination for package $PACKAGE_NAME: $DESTINATION does not exist. Making..."
-    mkdir -p "$DESTINATION" 2>/dev/null
+  echo "Destination for package $PACKAGE_NAME: $DESTINATION does not exist. Making..."
+  mkdir -p "$DESTINATION" 2>/dev/null
 fi
 
 # backup APK
 if [ "$APK_PATH_BASE" != "$NULL_MARKER" ]; then
-    appBackupDir="$DESTINATION/$PACKAGE_NAME.app"
-    mkdir -p "$appBackupDir"
+  appBackupDir="$DESTINATION/$PACKAGE_NAME.app"
+  mkdir -p "$appBackupDir"
 
-    cd "$APK_PATH_BASE" || { echo "Failed to cd into $APK_PATH_BASE" >&2; print_end_marker; exit 1; }
+  cd "$APK_PATH_BASE" || { echo "Failed to cd into $APK_PATH_BASE" >&2; print_end_marker; exit 1; }
 
-    echo
-    echo "Copy APKs"
-    echo
+  echo
+  echo "Copy APKs"
+  echo
 
-    for f in *.apk; do
-        cp -v "$f" "${appBackupDir}/$f"
-    done
+  for f in *.apk; do
+    cp -v "$f" "${appBackupDir}/$f"
+  done
 fi
 
 print_end_marker
--- app/src/main/res/raw/backup_data.sh
+++ app/src/main/res/raw/backup_data.sh
@@ -15,31 +15,31 @@ NULL_MARKER="$6"
 END_MARKER="$7"
 
 print_end_marker() {
-    echo "$END_MARKER"
-    echo "$END_MARKER" >&2
+  echo "$END_MARKER"
+  echo "$END_MARKER" >&2
 }
 
 echo "=== $APP_NAME (Data) ==="
 
 if [ ! -e "$DESTINATION" ]; then
-    echo "Destination for package $PACKAGE_NAME: $DESTINATION does not exist. Making..."
-    mkdir -p "$DESTINATION" 2>/dev/null
+  echo "Destination for package $PACKAGE_NAME: $DESTINATION does not exist. Making..."
+  mkdir -p "$DESTINATION" 2>/dev/null
 fi
 
 # backup data
 if [ "$DATA_PATH_BASE" != "$NULL_MARKER" ]; then
-    dataBackupLocation="$DESTINATION/$PACKAGE_NAME.tar.gz"
-    cd "$DATA_PATH_BASE" || { echo "Failed to cd into $DATA_PATH_BASE" >&2; print_end_marker; exit 1; }
+  dataBackupLocation="$DESTINATION/$PACKAGE_NAME.tar.gz"
+  cd "$DATA_PATH_BASE" || { echo "Failed to cd into $DATA_PATH_BASE" >&2; print_end_marker; exit 1; }
 
-    echo
-    echo "Backup private data"
-    echo
+  echo
+  echo "Backup private data"
+  echo
 
-    if [ "$IGNORE_CACHE" = "true" ]; then
-        tar -vczpf "$dataBackupLocation" "$DATA_PATH_BASE" --exclude="$DATA_PATH_BASE/cache"
-    else
-        tar -vczpf "$dataBackupLocation" "$DATA_PATH_BASE"
-    fi
+  if [ "$IGNORE_CACHE" = "true" ]; then
+    tar -vczpf "$dataBackupLocation" "$DATA_PATH_BASE" --exclude="$DATA_PATH_BASE/cache"
+  else
+    tar -vczpf "$dataBackupLocation" "$DATA_PATH_BASE"
+  fi
 fi
 
 print_end_marker
