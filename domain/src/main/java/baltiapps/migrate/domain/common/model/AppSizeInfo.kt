b00f1690740f7a94395d0f813c72c6c2e65de81e
SayantanRC <bin.sayantan@gmail.com>
2026-04-04 03:43:10 +0530

    refactor: make AppSizeInfo implement DataItem interface, use Nothing as ListItem type and throw exception in toListItem

--- domain/src/main/java/baltiapps/migrate/domain/common/model/AppSizeInfo.kt
+++ domain/src/main/java/baltiapps/migrate/domain/common/model/AppSizeInfo.kt
@@ -4,6 +4,13 @@ data class AppSizeInfo(
     val packageName: String,
     val bytesApk: Long,
     val bytesData: Long,
-) {
+
+    override val _id: String = packageName,
+    override val logInfo: String = packageName,
+): DataItem<Nothing> {
     val bytesTotal: Long get() = bytesApk + bytesData
+
+    override fun toListItem(): Nothing {
+        throw UnsupportedOperationException("AppSizeInfo does not support toListItem()")
+    }
 }
